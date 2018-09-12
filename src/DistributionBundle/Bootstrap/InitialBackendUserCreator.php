489da1c27ee401d909c65f362d18961a284c43f3
Stefan Kruppa <stefan@umulmrum.de>
2018-09-12 13:42:06 +0200

    ref #71: Add command for creating initial backend user

---
+++ src/DistributionBundle/Bootstrap/InitialBackendUserCreator.php
@@ -0,0 +1,202 @@
+<?php
+
+/*
+ * This file is part of the Chameleon System (https://www.chameleonsystem.com).
+ *
+ * (c) ESONO AG (https://www.esono.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ChameleonSystem\DistributionBundle\Bootstrap;
+
+use ChameleonSystem\CoreBundle\DatabaseAccessLayer\DatabaseAccessLayerFieldConfig;
+use ChameleonSystem\CoreBundle\Security\Password\PasswordHashGeneratorInterface;
+use ChameleonSystem\CoreBundle\Service\LanguageServiceInterface;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+class InitialBackendUserCreator
+{
+    /**
+     * @var LanguageServiceInterface
+     */
+    private $languageService;
+    /**
+     * @var Connection
+     */
+    private $databaseConnection;
+    /**
+     * @var DatabaseAccessLayerFieldConfig
+     */
+    private $fieldConfigDataAccess;
+    /**
+     * @var PasswordHashGeneratorInterface
+     */
+    private $passwordHashGenerator;
+
+    public function __construct(
+        LanguageServiceInterface $languageService,
+        Connection $databaseConnection,
+        DatabaseAccessLayerFieldConfig $fieldConfigDataAccess,
+        PasswordHashGeneratorInterface $passwordHashGenerator
+    ) {
+        $this->languageService = $languageService;
+        $this->databaseConnection = $databaseConnection;
+        $this->fieldConfigDataAccess = $fieldConfigDataAccess;
+        $this->passwordHashGenerator = $passwordHashGenerator;
+    }
+
+    public function create(InputInterface $input, OutputInterface $output, QuestionHelper $questionHelper): void
+    {
+        $query = "SELECT 1 FROM `cms_user` WHERE `login` != 'www' LIMIT 1";
+        $hasUser = $this->databaseConnection->fetchColumn($query);
+        if ('1' === $hasUser) {
+            throw new \LogicException('This command can only be used to create an initial user, but at least one user already exists.');
+        }
+
+        $name = $this->getUsername($questionHelper, $input, $output);
+        $password = $this->getPassword($questionHelper, $input, $output);
+
+        $this->doCreate($name, $password);
+    }
+
+    private function getUsername(QuestionHelper $questionHelper, InputInterface $input, OutputInterface $output): string
+    {
+        $username = \getenv('APP_INITIAL_BACKEND_USER_NAME');
+        if (false === $username) {
+            $question = new Question('Please enter the name of the initial backend user (may be everything but "www"): ');
+            $username = $questionHelper->ask($input, $output, $question);
+        }
+
+        if ('www' === $username) {
+            throw new \InvalidArgumentException('The name of the user must not be "www".');
+        }
+
+        return $username;
+    }
+
+    private function getPassword(QuestionHelper $questionHelper, InputInterface $input, OutputInterface $output): string
+    {
+        $password = \getenv('APP_INITIAL_BACKEND_USER_PASSWORD');
+        $minimumLength = $this->getMinimumPasswordLength();
+        if (false === $password) {
+            $password = $this->askPassword($questionHelper, $input, $output, $minimumLength);
+        } else {
+            if (\mb_strlen(\trim($password)) < $minimumLength) {
+                throw new \InvalidArgumentException(sprintf('Password is too short. Please use at least %s characters.', $minimumLength));
+            }
+        }
+
+        return $this->passwordHashGenerator->hash($password);
+    }
+
+    private function askPassword(QuestionHelper $questionHelper, InputInterface $input, OutputInterface $output, int $minimumLength): ?string
+    {
+        $question = new Question(\sprintf('Please enter the password of the initial backend user (at least %s characters): ', $minimumLength));
+        $question->setHidden(true);
+        $question->setValidator(function ($value) use ($minimumLength) {
+            if (\mb_strlen(\trim($value)) < $minimumLength) {
+                throw new \InvalidArgumentException(sprintf('Password is too short. Please use at least %s characters.', $minimumLength));
+            }
+
+            return $value;
+        });
+
+        return $questionHelper->ask($input, $output, $question);
+    }
+
+    private function getMinimumPasswordLength(): int
+    {
+        $definition = $this->fieldConfigDataAccess->GetFieldDefinition('cms_user', 'crypted_pw');
+        $minimumLength = $definition->GetFieldtypeConfigKey('minimumLength');
+        if (false === \is_numeric($minimumLength)) {
+            return \TCMSFieldPassword::DEFAULT_MINIMUM_PASSWORD_LENGTH;
+        }
+
+        return (int) $minimumLength;
+    }
+
+    private function doCreate(string $name, string $password): void
+    {
+        $userId = \TTools::GetUUID();
+
+        $this->addUser($userId, $name, $password);
+        $this->addUserRoles($userId);
+        $this->addUserGroups($userId);
+    }
+
+    private function addUser(string $userId, string $name, string $password): void
+    {
+        $languageEn = $this->languageService->getLanguageFromIsoCode('en');
+
+        $this->databaseConnection->insert('cms_user', [
+            'id' => $userId,
+            'cmsident' => null,
+            'login' => $name,
+            'crypted_pw' => $password,
+            'company' => '',
+            'department' => '',
+            'firstname' => '',
+            'name' => $name,
+            'city' => '',
+            'email' => '',
+            'tel' => '',
+            'fax' => '',
+            'cms_language_id' => $languageEn->id,
+            'user_tbl_conf_hidden' => '',
+            'languages' => 'en',
+            'images' => '1',
+            'cms_current_edit_language' => 'en',
+            'allow_cms_login' => '1',
+            'cms_workflow_transaction_id' => '',
+            'task_show_count' => '5',
+            'is_system' => '1',
+            'show_as_rights_template' => '1',
+        ]);
+
+        $this->databaseConnection->insert('cms_user_cms_language_mlt', [
+            'source_id' => $userId,
+            'target_id' => $languageEn->id,
+            'entry_sort' => 1,
+        ]);
+    }
+
+    private function addUserRoles(string $userId): void
+    {
+        $userRoleList = $this->databaseConnection->fetchAll('SELECT `id` FROM `cms_role`');
+
+        $query = 'INSERT INTO `cms_user_cms_role_mlt` (`source_id`, `target_id`, `entry_sort`) VALUES (?, ?, ?)';
+        $statement = $this->databaseConnection->prepare($query);
+        $i = 0;
+        foreach ($userRoleList as $subList) {
+            $roleId = $subList['id'];
+            $statement->execute([
+                $userId,
+                $roleId,
+                $i++,
+            ]);
+        }
+    }
+
+    private function addUserGroups(string $userId): void
+    {
+        $userGroupList = $this->databaseConnection->fetchAll('SELECT `id` FROM `cms_usergroup`');
+
+        $query = 'INSERT INTO `cms_user_cms_usergroup_mlt` (`source_id`, `target_id`, `entry_sort`) VALUES (?, ?, ?)';
+        $statement = $this->databaseConnection->prepare($query);
+        $i = 0;
+        foreach ($userGroupList as $subList) {
+            $userGroupId = $subList['id'];
+            $statement->execute([
+                $userId,
+                $userGroupId,
+                $i++,
+            ]);
+        }
+    }
+}
---
+++ src/DistributionBundle/Command/CreateInitialBackendUserCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Chameleon System (https://www.chameleonsystem.com).
+ *
+ * (c) ESONO AG (https://www.esono.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ChameleonSystem\DistributionBundle\Command;
+
+use ChameleonSystem\CoreBundle\Service\LanguageServiceInterface;
+use ChameleonSystem\DistributionBundle\Bootstrap\InitialBackendUserCreator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Console command for creating backend users.
+ */
+class CreateInitialBackendUserCommand extends Command
+{
+    /**
+     * @var LanguageServiceInterface
+     */
+    private $languageService;
+    /**
+     * @var InitialBackendUserCreator
+     */
+    private $initialBackendUserCreator;
+
+    public function __construct(InitialBackendUserCreator $initialBackendUserCreator)
+    {
+        parent::__construct('chameleon_system:bootstrap:create_initial_backend_user');
+        $this->initialBackendUserCreator = $initialBackendUserCreator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setDefinition(array())
+            ->setDescription('Creates a backend user.')
+            ->setHelp(<<<EOF
+The <info>%command.name%</info> creates an initial Chameleon backend user, either by accepting arguments from the environment
+or interactively. A user created by this command is a basic stub that is minimally configured to be able to log into the backend.
+The user should be edited in the backend thereafter.
+Possible environment variables:
+- APP_INITIAL_USER_NAME: The user name
+- APP_INITIAL_USER_PASSWORD The password
+EOF
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->initialBackendUserCreator->create($input, $output, $this->getHelper('question'));
+    }
+
+    public function setLanguageService(LanguageServiceInterface $languageService): void
+    {
+        $this->languageService = $languageService;
+    }
+}
