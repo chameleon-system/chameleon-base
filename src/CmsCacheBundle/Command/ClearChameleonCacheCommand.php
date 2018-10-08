568cacd59b02bbd35c8f86fffecae9d173bbff0f
Ulrich Kuhn <ulrich.kuhn@esono.de>
2018-10-08 14:51:33 +0200

    ref #114: Add cache clear command

---
+++ src/CmsCacheBundle/Command/ClearChameleonCacheCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Chameleon System (https://www.chameleonsystem.com).
+ *
+ * (c) ESONO AG (https://www.esono.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ChameleonSystem\CmsCacheBundle\Command;
+
+use esono\pkgCmsCache\CacheInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\LogicException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearChameleonCacheCommand extends Command
+{
+    /**
+     * @var CacheInterface
+     */
+    private $cache;
+
+    /**
+     * @param CacheInterface $cache
+     * @param null|string    $name
+     *
+     *  @throws LogicException When the command name is empty
+     */
+    public function __construct(CacheInterface $cache, $name = null)
+    {
+        parent::__construct($name);
+
+        $this->cache = $cache;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this->setName('chameleon_system:cache:clear')
+            ->setDescription('Clears the chameleon cache.');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->cache->clearAll();
+
+        $output->writeln('<info>OK</info>');
+
+        return 0;
+    }
+}
