10232733a6d96035bfddd616c07d3b17d3abb90b
Ulrich Kuhn <ulrich.kuhn@esono.de>
2018-10-09 14:36:19 +0200

    ref #118: Add commands to activate/deactivate the maintenance mode

---
+++ src/CoreBundle/Command/DeactivateMaintenanceModeCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Chameleon System (https://www.chameleonsystem.com).
+ *
+ * (c) ESONO AG (https://www.esono.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ChameleonSystem\CoreBundle\Command;
+
+use ChameleonSystem\CoreBundle\Exception\MaintenanceModeErrorException;
+use ChameleonSystem\CoreBundle\Service\MaintenanceModeServiceInterface;
+use ChameleonSystem\CoreBundle\ServiceLocator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DeactivateMaintenanceModeCommand extends Command
+{
+    public function __construct($name = null)
+    {
+        parent::__construct('chameleon_system:maintenance_mode:deactivate');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Deactivates the maintenance mode')
+            ->setHelp(<<<EOF
+The <info>%command.name%</info> command deactivates the maintenance mode.
+EOF
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /**
+         * @var $maintenanceModeService MaintenanceModeServiceInterface
+         */
+        $maintenanceModeService = ServiceLocator::get('chameleon_system_core.maintenance_mode_service');
+
+        try {
+            $maintenanceModeService->deactivate();
+        } catch (MaintenanceModeErrorException $exception) {
+            $output->writeln(sprintf('Maintenance mode could not be deactivated: %s', $exception->getMessage()));
+
+            return 1;
+        }
+
+        return 0;
+    }
+}
