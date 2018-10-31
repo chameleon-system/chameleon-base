d236c639044456227e7692646afad2a38f6083d2
Sven Jansen <sven.jansen@esono.de>
2018-10-31 13:27:39 +0100

    Revert "ref #178: remove unused iconFont directory because of possible license issues"
    This reverts commit 4289745

--- src/CoreBundle/private/modules/MTHeader/MTHeader.class.php
+++ src/CoreBundle/private/modules/MTHeader/MTHeader.class.php
@@ -767,6 +767,8 @@ class MTHeader extends TCMSModelBase
         $aIncludes[] = '<link href="/chameleon/blackbox/bootstrap/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css" />';
         $aIncludes[] = '<link href="/chameleon/blackbox/bootstrap/css/bootstrap-theme.min.css" media="screen" rel="stylesheet" type="text/css" />';
         $aIncludes[] = '<link href="'.TGlobal::GetPathTheme().'/css/bootstrap.css" media="screen" rel="stylesheet" type="text/css" />';
+        $aIncludes[] = '<link href="/chameleon/blackbox/iconFonts/foundation/foundation-icons.css" media="screen" rel="stylesheet" type="text/css" />';
+        $aIncludes[] = '<link href="/chameleon/blackbox/iconFonts/ionicons/ionicons.css" media="screen" rel="stylesheet" type="text/css" />';
 
         return $aIncludes;
     }
