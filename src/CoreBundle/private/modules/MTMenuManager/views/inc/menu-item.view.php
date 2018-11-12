a10a4acbf84d7f1b0fdb14cb076a15678c0d79f4
Sven Jansen <sven.jansen@esono.de>
2018-11-12 10:53:22 +0100

    ref #185: fix code style

--- src/CoreBundle/private/modules/MTMenuManager/views/inc/menu-item.view.php
+++ src/CoreBundle/private/modules/MTMenuManager/views/inc/menu-item.view.php
@@ -6,7 +6,7 @@ while ($oMenu = $data[$menuColName]->Next()) {
         ?>
         <div class="card mb-3">
             <div class="card-header">
-                <h5 class="card-title mb-0"><?=TGlobal::OutHTML($oMenu->GetName()) ?></h5>
+                <h5 class="card-title mb-0"><?= TGlobal::OutHTML($oMenu->GetName()); ?></h5>
             </div>
             <div class="card-body p-2">
                 <nav class="nav flex-column">
@@ -14,8 +14,7 @@ while ($oMenu = $data[$menuColName]->Next()) {
                     while ($oMenuItem = $oMenu->oMenuItems->Next()) {
                         /** @var $oMenuItem TCMSMenuItem */
                         echo $oMenuItem->GetLink();
-                    }
-                    ?>
+                    } ?>
                 </nav>
             </div>
         </div>
