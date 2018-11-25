c920963bfb4509d9463c25979cdbef0c0d06a6af
Sven Jansen <sven.jansen@esono.de>
2018-11-25 22:19:47 +0100

    ref #99: style main menu items

--- src/CoreBundle/private/modules/MTMenuManager/views/inc/menu-item.view.php
+++ src/CoreBundle/private/modules/MTMenuManager/views/inc/menu-item.view.php
@@ -8,12 +8,22 @@ while ($oMenu = $data[$menuColName]->Next()) {
             <div class="card-header">
                 <h5 class="card-title mb-0"><?= TGlobal::OutHTML($oMenu->GetName()); ?></h5>
             </div>
-            <div class="card-body p-2">
+            <div class="card-body p-0">
                 <nav class="nav flex-column">
                     <?php
+                    $count = 0;
                     while ($oMenuItem = $oMenu->oMenuItems->Next()) {
+                        $count++;
+
+                        $rowClass = 'bg-light';
+                        if ($count %2) {
+                            $rowClass = '';
+                        }
+
+                        echo '<div class="pl-2 '.$rowClass.'">';
                         /** @var $oMenuItem TCMSMenuItem */
                         echo $oMenuItem->GetLink();
+                        echo '</div>';
                     } ?>
                 </nav>
             </div>
