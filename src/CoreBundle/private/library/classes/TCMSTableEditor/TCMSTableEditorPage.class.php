98cda9767ca81478efc10bc1b71e31b8431fde7e
Ulrich Kuhn <ulrich.kuhn@esono.de>
2018-12-06 14:44:33 +0100

    ref #230: Do not undermine change log by second save

--- src/CoreBundle/private/library/classes/TCMSTableEditor/TCMSTableEditorPage.class.php
+++ src/CoreBundle/private/library/classes/TCMSTableEditor/TCMSTableEditorPage.class.php
@@ -64,8 +64,13 @@ class TCMSTableEditorPage extends TCMSTableEditor
      */
     protected function PostSaveHook(&$oFields, &$oPostTable)
     {
-        $this->UpdatePageNaviBreadCrumb();
         parent::PostSaveHook($oFields, $oPostTable);
+
+        $this->UpdatePageNaviBreadCrumb();
+        // TODO With this reversed order this save cannot be part of the change log...
+        // But otherwise this second save overwrites the (new) values of the first general save
+
+        // Also note vendor/chameleon-system/chameleon-base/src/CmsChangeLogBundle/src/esono/pkgcmschangelog/TCMSTableEditor/TCMSTableEditorChangeLog.class.php:100
     }
 
     /**
