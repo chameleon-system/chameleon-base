86927cd87b9093934083d7a86390062f54381ac7
Ulrich Kuhn <ulrich.kuhn@esono.de>
2018-12-12 09:11:39 +0100

    ref #92: Remove special channel for outdated pkgcmscore

--- src/PkgCoreBundle/objects/TPkgCmsCoreSendToHost.class.php
+++ src/PkgCoreBundle/objects/TPkgCmsCoreSendToHost.class.php
@@ -529,6 +529,6 @@ class TPkgCmsCoreSendToHost
 
     private function getLogger(): LoggerInterface
     {
-        return ServiceLocator::get('monolog.logger.pkg_core');
+        return ServiceLocator::get('logger');
     }
 }
