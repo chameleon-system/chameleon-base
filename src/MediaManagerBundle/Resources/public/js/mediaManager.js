0986cf59bb3b4d9692cdf0e564be2193a05aaa85
Sven Jansen <sven.jansen@esono.de>
2018-12-20 00:27:57 +0100

    ref #248: fix height of layover container with iframes that have no initial height

--- src/MediaManagerBundle/Resources/public/js/mediaManager.js
+++ src/MediaManagerBundle/Resources/public/js/mediaManager.js
@@ -1014,7 +1014,13 @@
             var layover = self.openLayover(title, '');
 
             if (typeof height === 'undefined') {
-                height = layover.height() - layover.find('.title').height() - 50;
+
+                var height = layover.height();
+                if (height < 100) {
+                    height = $('#edit-container').height();
+                }
+
+                height = height - (layover.find('.title').height() - 50);
             }
             iframe.css({'width': '100%', 'height': height}).appendTo(layover);
 
