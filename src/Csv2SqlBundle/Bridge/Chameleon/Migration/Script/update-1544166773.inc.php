db8aa2c9d17934652db32093c5fb1434671e52c3
Ulrich Kuhn <ulrich.kuhn@esono.de>
2019-01-08 10:35:17 +0100

    ref #92: Add English text update

--- src/Csv2SqlBundle/Bridge/Chameleon/Migration/Script/update-1544166773.inc.php
+++ src/Csv2SqlBundle/Bridge/Chameleon/Migration/Script/update-1544166773.inc.php
@@ -37,3 +37,34 @@ Beispiel: "/wawi/attribute/incoming" -> Import aller CSV-Dateien aus dem Ordner 
 ;
 TCMSLogChange::update(__LINE__, $data);
 
+$data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
+    ->setFields([
+        '049_helptext' => 'Possible entries:
+- filename
+- directory
+- patterns
+
+All directories are relative to app/cmsdata/.
+
+A note on the folder structure:
+Files to be imported must always be located in an "incoming" folder. Parallel to "incoming" there should be "working","archive" and "logs".
+
+Example: "/wawi/incoming/my.csv" requires the following folders:
+
+./app/cmsdata/wawi/incoming - Here is the file my.csv which can be imported.
+./app/cmsdata/wawi/working - The file is moved here, when processed by the system.
+./app/cmsdata/wawi/archive - The file is moved to the archive when processing is complete (the file is prefixed with YYYYMMDD_HHMMSS- )
+
+The same rule applies if you want to import all files in a folder instead of one file.
+
+Example: "/wawi/attribute/incoming" -> Import of all CSV files from the folder ./app/cmsdata/wawi/attribute/incoming requires that the following folders exist:
+./app/cmsdata/wawi/attribute/incoming
+./app/cmsdata/wawi/attribute/working
+./app/cmsdata/wawi/attribute/archive
+',
+    ])
+    ->setWhereEquals([
+        'id' => TCMSLogChange::GetTableFieldId(TCMSLogChange::GetTableId('pkg_csv2sql'), 'source'),
+    ])
+;
+TCMSLogChange::update(__LINE__, $data);
