89d23f00f2b82c2cb16fbaa3a3204e6ce192c506
Sven Jansen <sven.jansen@esono.de>
2019-01-15 14:53:45 +0100

    ref #99: removed irrelevant substr

--- src/CoreBundle/Resources/public/javascript/tableEditor.js
+++ src/CoreBundle/Resources/public/javascript/tableEditor.js
@@ -720,7 +720,7 @@ $(document).ready(function () {
 
 CHAMELEON.CORE.MTTableEditor.initTabs = function () {
     var url = document.URL;
-    var hash = window.location.hash.substr(0);
+    var hash = window.location.hash;
 
     $('.nav-tabs').find('li a').each(function (key, tabLinkItem) {
         var $tabLinkItem = $(tabLinkItem);
@@ -760,7 +760,7 @@ CHAMELEON.CORE.MTTableEditor.initDateTimePickers  = function () {
             } else {
                 var cmsDate = moment.format('YYYY-MM-DD HH:mm:ss');
             }
-            // We need a SQL-date-format for BC reasons.
+            // We need a SQL date format for BC reasons.
             $('input[name=' + id + ']').val(cmsDate);
         });
     });
