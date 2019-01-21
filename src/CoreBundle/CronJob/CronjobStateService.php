3253f154be60c0e15ae642639ddbf74f99369dee
Ulrich Kuhn <ulrich.kuhn@esono.de>
2019-01-21 15:06:16 +0100

    ref #134: Add license header

--- src/CoreBundle/CronJob/CronjobStateService.php
+++ src/CoreBundle/CronJob/CronjobStateService.php
@@ -1,7 +1,15 @@
 <?php
 
-namespace ChameleonSystem\CoreBundle\CronJob;
+/*
+ * This file is part of the Chameleon System (https://www.chameleonsystem.com).
+ *
+ * (c) ESONO AG (https://www.esono.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+namespace ChameleonSystem\CoreBundle\CronJob;
 
 use ChameleonSystem\CoreBundle\Exception\CronjobHandlingException;
 use Doctrine\DBAL\Connection;
