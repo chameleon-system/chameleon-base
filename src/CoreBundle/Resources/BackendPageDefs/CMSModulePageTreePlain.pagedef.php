3a37412ec30be1180f41f3a28b1e451aac20c5ed
Stefan Kruppa <stefan@umulmrum.de>
2019-01-28 14:55:47 +0100

    ref #100: Display navigation tree inline

--- src/CoreBundle/Resources/BackendPageDefs/CMSModulePageTreePlain.pagedef.php
+++ src/CoreBundle/Resources/BackendPageDefs/CMSModulePageTreePlain.pagedef.php
@@ -1,6 +1,9 @@
 <?php
 
-$layoutTemplate = 'ajaxTreePlain';
-$moduleList = array('module' => array('model' => 'CMSModulePageTree', 'view' => 'standard'));
+$layoutTemplate = 'default';
+$moduleList = array('contentmodule' => array('model' => 'CMSModulePageTree', 'view' => 'standard'));
 
 addDefaultPageTitle($moduleList);
+addDefaultHeader($moduleList);
+addDefaultBreadcrumb($moduleList);
+addDefaultSidebar($moduleList);
