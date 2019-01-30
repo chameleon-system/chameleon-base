8c849d0a55eb699d049b1041283dd7afd9661a16
Sven Jansen <sven.jansen@esono.de>
2019-01-30 10:24:56 +0100

    ref #99: deprecate orphaned view

--- src/CoreBundle/private/modules/esono_cmsversioninfo_MTVersionInfo/views/standard.view.php
+++ src/CoreBundle/private/modules/esono_cmsversioninfo_MTVersionInfo/views/standard.view.php
@@ -1,10 +1,12 @@
 <?php
 /**
-* @var $updates array
-* @var $deploys array
-* @var $logEntryList array
-* @var $logEntry \esono\cmsversioninfo\Commit
-*/
+ * @deprecated since 6.3.0
+ *
+ * @var $updates array
+ * @var $deploys array
+ * @var $logEntryList array
+ * @var $logEntry \esono\cmsversioninfo\Commit
+ */
 ?>
 
 <form method="post" action="<?=PATH_CMS_CONTROLLER; ?>" accept-charset="UTF-8" style="float:left;">
@@ -20,7 +22,7 @@
 
 <div style="clear: both"></div>
     <h1>Updates since last refresh</h1>
-    <div class="contentbox">
+    <div class="">
         <div class="contentcontainer">
             <?php
              foreach ($updates as $name => $logEntryList) {
