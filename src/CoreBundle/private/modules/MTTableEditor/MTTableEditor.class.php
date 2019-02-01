f80d7b3c7cf6ea25c31909f43c828da6ba3b89e8
August S. Freytag <august.freytag@esono.de>
2019-02-01 14:07:55 +0100

    ref #45220: Add include for version history client module to table edit.

--- src/CoreBundle/private/modules/MTTableEditor/MTTableEditor.class.php
+++ src/CoreBundle/private/modules/MTTableEditor/MTTableEditor.class.php
@@ -519,6 +519,7 @@ class MTTableEditor extends TCMSModelBase
         $aIncludes[] = '<link href="'.TGlobal::GetPathTheme().'/css/contextmenu.css" rel="stylesheet" type="text/css" />';
         $aIncludes[] = '<link href="'.TGlobal::GetPathTheme().'/css/tableeditcontainer.css" rel="stylesheet" type="text/css" />';
         $aIncludes[] = '<script src="'.TGlobal::GetStaticURLToWebLib('/javascript/tableEditor.js').'" type="text/javascript"></script>';
+        $aIncludes[] = '<script src="'.TGlobal::GetStaticURLToWebLib('/javascript/versionHistoryOverlay.js').'" type="text/javascript"></script>';
         $aIncludes[] = '<script src="'.TGlobal::GetStaticURLToWebLib('/javascript/jquery/WayfarerTooltip/WayfarerTooltip.js').'" type="text/javascript"></script>';
         $aIncludes[] = '<link href="'.TGlobal::GetPathTheme().'/css/tooltip.css" rel="stylesheet" type="text/css" />';
         $aIncludes[] = '<link href="'.TGlobal::GetStaticURLToWebLib('/components/select2.v4/css/select2.min.css').'" media="screen" rel="stylesheet" type="text/css" />';
