20dc7bdbf797d05029f970f533ddac92dff6ad42
August S. Freytag <august.freytag@esono.de>
2019-02-04 13:01:22 +0100

    ref #45220: Unserialize change log item data, use same for display outp.

--- src/CoreBundle/private/library/functions/callback_functions/gcf_changeLogItemAttributes.php
+++ src/CoreBundle/private/library/functions/callback_functions/gcf_changeLogItemAttributes.php
@@ -2,6 +2,8 @@
 
 function gcf_changeLogItemAttributes($field, $row, $fieldName)
 {
-    $encodedPayload = base64_encode($row[$fieldName]);
-    return sprintf('<span data-field-restorable-value="%s">%s</span>', $encodedPayload, $field);
+    $originalFieldValue = unserialize($row[$fieldName], ['allowed_classes' => []]);
+    $encodedPayload = base64_encode($originalFieldValue);
+
+    return sprintf('<span data-field-restorable-value="%s">%s</span>', $encodedPayload, $originalFieldValue);
 }
