3dd58479059ac33ff3c138308ed24a00cbc1d4b7
Carmen Kleis <carmen.kleis@esono.de>
2019-02-07 13:09:36 +0100

    ref #298: sanityCheck styling

--- src/SanityCheckChameleonBundle/objects/BackendModules/chameleon_system_sanity_check_chameleon.module.cms_sanity_check/views/standard.view.php
+++ src/SanityCheckChameleonBundle/objects/BackendModules/chameleon_system_sanity_check_chameleon.module.cms_sanity_check/views/standard.view.php
@@ -8,51 +8,53 @@ use Symfony\Component\Translation\TranslatorInterface;
  */
 $translator = ServiceLocator::get('translator');
 ?>
-<div class="CMSInterfacePopup">
+<div class="card CMSInterfacePopup">
+    <div class="card-header">
+        <h3 class="mb-0"><i class="fas fa-bug mr-2"></i>SanityCheck<h3>
+    </div>
+    <div class="card-body">
+        <form method="post" action="<?= PATH_CMS_CONTROLLER; ?>" accept-charset="UTF-8">
+            <input type="hidden" name="pagedef" value="<?= TGlobal::OutHTML($data['pagedef']); ?>"/>
+            <input type="hidden" name="module_fnc[<?= TGlobal::OutHTML($data['sModuleSpotName']); ?>]" value=""/>
+            <input type="hidden" name="_pagedefType" value="<?= $data['_pagedefType']; ?>"/>
 
-    <h2><?= TGlobal::OutHTML($translator->trans('label.perform_single_check', [], 'chameleon_system_sanitycheck')); ?></h2>
+            <div class="form-group">
+                <label class="font-weight-bold">
+                    <?= TGlobal::OutHTML($translator->trans('label.perform_single_check', [], 'chameleon_system_sanitycheck')); ?>
+                </label>
+                <select class="form-control" name="singleCheck[]" multiple="multiple" size="8">
+                    <?php
+                    foreach ($data['checks'] as $check) {
+                        echo '<option value="'.$check[0].'">'.(null !== $check[1] ? $data['translator']->trans(
+                                $check[1],
+                                array(),
+                                'chameleon_system_sanitycheck'
+                            ) : $check[0]).'</option>';
+                    }
+                    ?>
+                </select>
+                <input class="btn btn-primary mt-2" type="submit" value="<?= TGlobal::OutHTML($translator->trans('label.perform_check', [], 'chameleon_system_sanitycheck')); ?>" />
+            </div>
+        </form>
 
-    <form method="post" action="<?= PATH_CMS_CONTROLLER; ?>" accept-charset="UTF-8">
-        <input type="hidden" name="pagedef" value="<?= TGlobal::OutHTML($data['pagedef']); ?>"/>
-        <input type="hidden" name="module_fnc[<?= TGlobal::OutHTML($data['sModuleSpotName']); ?>]" value=""/>
-        <input type="hidden" name="_pagedefType" value="<?= $data['_pagedefType']; ?>"/>
-        <select name="singleCheck[]" multiple="multiple" size="8">
+        <form method="post" action="<?= PATH_CMS_CONTROLLER; ?>" accept-charset="UTF-8">
+            <input type="hidden" name="pagedef" value="<?= TGlobal::OutHTML($data['pagedef']); ?>"/>
+            <input type="hidden" name="module_fnc[<?= TGlobal::OutHTML($data['sModuleSpotName']); ?>]" value=""/>
+            <input type="hidden" name="_pagedefType" value="<?= $data['_pagedefType']; ?>"/>
 
-            <?php
-            foreach ($data['checks'] as $check) {
-                echo '<option value="'.$check[0].'">'.(null !== $check[1] ? $data['translator']->trans(
-                        $check[1],
-                        array(),
-                        'chameleon_system_sanitycheck'
-                    ) : $check[0]).'</option>';
-            }
-            ?>
-            <input
-                type="submit"
-                value="<?= TGlobal::OutHTML($translator->trans('label.perform_check', [], 'chameleon_system_sanitycheck')); ?>"
-                style="margin-left: 1em;"
-                />
-        </select>
-    </form>
-
-    <h2><?= TGlobal::OutHTML($translator->trans('label.perform_checks_for_bundle', [], 'chameleon_system_sanitycheck')); ?></h2>
-
-    <form method="post" action="<?= PATH_CMS_CONTROLLER; ?>" accept-charset="UTF-8">
-        <input type="hidden" name="pagedef" value="<?= TGlobal::OutHTML($data['pagedef']); ?>"/>
-        <input type="hidden" name="module_fnc[<?= TGlobal::OutHTML($data['sModuleSpotName']); ?>]" value=""/>
-        <input type="hidden" name="_pagedefType" value="<?= $data['_pagedefType']; ?>"/>
-        <select name="bundleCheck[]" multiple="multiple" size="8">
-
-            <?php
-            foreach ($data['bundlesWithChecks'] as $bundle) {
-                echo '<option>'.$bundle.'</option>';
-            }
-            ?>
-            <input
-                type="submit"
-                value="<?= TGlobal::OutHTML($translator->trans('label.perform_checks', [], 'chameleon_system_sanitycheck')); ?>"
-                style="margin-left: 1em;"
-                />
-        </select>
-    </form>
+            <div class="form-group">
+                <label class="font-weight-bold">
+                    <?= TGlobal::OutHTML($translator->trans('label.perform_checks_for_bundle', [], 'chameleon_system_sanitycheck')); ?>
+                </label>
+                <select class="form-control" name="bundleCheck[]" multiple="multiple" size="8">
+                    <?php
+                    foreach ($data['bundlesWithChecks'] as $bundle) {
+                        echo '<option>'.$bundle.'</option>';
+                    }
+                    ?>
+                </select>
+                <input class="btn btn-primary mt-2" type="submit" value="<?= TGlobal::OutHTML($translator->trans('label.perform_checks', [], 'chameleon_system_sanitycheck')); ?>" />
+            </div>
+        </form>
+    </div>
 </div>
