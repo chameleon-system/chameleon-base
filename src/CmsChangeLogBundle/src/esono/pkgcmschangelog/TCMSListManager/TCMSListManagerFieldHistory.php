88a7bb897519180e3eadd1234d55ddcc5af62c47
August S. Freytag <august.freytag@esono.de>
2019-02-08 11:33:25 +0100

    ref #44248: Remove doc block comment for isolated return type

--- src/CmsChangeLogBundle/src/esono/pkgcmschangelog/TCMSListManager/TCMSListManagerFieldHistory.php
+++ src/CmsChangeLogBundle/src/esono/pkgcmschangelog/TCMSListManager/TCMSListManagerFieldHistory.php
@@ -52,9 +52,6 @@ class TCMSListManagerFieldHistory extends TCMSListManagerFullGroupTable
         );
     }
 
-    /**
-     * @return string
-     */
     protected function _GetRecordClickJavaScriptFunctionName(): string
     {
         return 'restoreFieldValueVersion';
