29390b371a8494760fb15e67df9940a6bddb7c6d
Carmen Kleis <carmen.kleis@esono.de>
2019-02-11 08:58:15 +0100

    ref #301: navigation tree: page edit: styling buttons

--- src/CoreBundle/private/modules/CMSTemplateEngine/views/parts/navi.inc.php
+++ src/CoreBundle/private/modules/CMSTemplateEngine/views/parts/navi.inc.php
@@ -1,9 +1,11 @@
-<div class="btn-group">
+<div class="row button-element">
         <?php
         $data['oMenuItems']->GoToStart();
         /** @var $oMenuItem TCMSTableEditorMenuItem */
         while ($oMenuItem = $data['oMenuItems']->Next()) {
-            echo $oMenuItem->GetMenuItemHTML();
+            echo '<div class="button-item col-12 col-sm-6 col-md-4 col-lg-auto">';
+                echo $oMenuItem->GetMenuItemHTML();
+            echo '</div>';
         }
         ?>
 </div>
