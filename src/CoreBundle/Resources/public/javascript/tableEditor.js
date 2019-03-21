cdd8b4ed1c829d83ac8cb5e5dbadd526e345b954
Sven Jansen <sven.jansen@esono.de>
2019-03-21 14:04:20 +0100

    ref #328: return to list page after save if inside a modal dialog

--- src/CoreBundle/Resources/public/javascript/tableEditor.js
+++ src/CoreBundle/Resources/public/javascript/tableEditor.js
@@ -579,6 +579,10 @@ function SaveViaAjaxCallback(data, statusText) {
                 sCurrentRecordName = data.name;
             }
         }
+
+        if (self !== top) {
+            window.history.go(-1);
+        }
     } else {
         toasterMessage(CHAMELEON.CORE.i18n.Translate('chameleon_system_core.js.error_save'), 'ERROR');
     }
