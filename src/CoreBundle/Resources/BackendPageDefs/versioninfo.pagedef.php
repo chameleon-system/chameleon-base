82d494b5f50f3751bd445d9950ddf445df2fda8c
Sven Jansen <sven.jansen@esono.de>
2019-03-28 17:04:27 +0100

    ref #192: delete unused version info module files

--- src/CoreBundle/Resources/BackendPageDefs/versioninfo.pagedef.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @deprecated since 6.3.0 - not used anymore.
- */
-$layoutTemplate = 'popup_window_iframe';
-$moduleList = array('contentmodule' => array('model' => 'esono\cmsversioninfo\MTVersionInfo', 'view' => 'standard', '_suppressHistory' => true));
-
-addDefaultPageTitle($moduleList);
