b388e228c8cb1a8f3295553617074c8eef234b14
Sven Jansen <sven.jansen@esono.de>
2019-04-03 13:35:53 +0200

    ref #370: fix spelling in updates, cleanup, add upgrade note for icons, add fallback icon css class

--- src/CoreBundle/Bridge/Chameleon/Migration/Script/update-1554106531.inc.php
+++ src/CoreBundle/Bridge/Chameleon/Migration/Script/update-1554106531.inc.php
@@ -19,16 +19,29 @@ $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
 ;
 TCMSLogChange::update(__LINE__, $data);
 
+$data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
+    ->setFields([
+//      'name' => 'icon_list',
+        '049_helptext' => '@deprecated since 7.0.0 - no longer used - use icon_font_css_class instead',
+    ])
+    ->setWhereEquals([
+        'id' => $fieldId,
+    ])
+;
+TCMSLogChange::update(__LINE__, $data);
+
 $query = "ALTER TABLE `cms_tpl_module`
                      CHANGE `icon_list`
                             `icon_list` VARCHAR(255) DEFAULT 'application.png' NOT NULL COMMENT 'Icon: @deprecated since 7.0.0 - no longer used - use icon_font_css_class instead'";
 TCMSLogChange::RunQuery(__LINE__, $query);
 
+$recordId = TCMSLogChange::createUnusedRecordId();
+
 $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
   ->setFields([
       'cms_tbl_conf_id' => TCMSLogChange::GetTableId('cms_tpl_module'),
-      'name' => 'new_field',
-      'translation' => '',
+      'name' => 'icon_font_css_class',
+      'translation' => 'Icon-Font CSS-Klasse',
       'cms_field_type_id' => TCMSLogChange::GetFieldType('CMSFIELD_STRING'),
       'cms_tbl_field_tab' => '',
       'isrequired' => '0',
@@ -42,11 +55,11 @@ $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
       'restrict_to_groups' => '0',
       'field_width' => '',
       'position' => '0',
-      '049_helptext' => '',
+      '049_helptext' => 'In diesem Feld kann ein Font-Icon angegeben werden. Tragen Sie hier den Klassennamen ein, z.B. für Font Awesome: fas fa-check',
       'row_hexcolor' => '',
       'is_translatable' => '0',
       'validation_regex' => '',
-      'id' => '6a8eb0c7-bb06-e930-f29b-d6e9073fc1d2',
+      'id' => $recordId,
   ])
 ;
 TCMSLogChange::insert(__LINE__, $data);
@@ -55,27 +68,14 @@ $query = "ALTER TABLE `cms_tpl_module`
                         ADD `icon_font_css_class` VARCHAR(255) NOT NULL COMMENT 'Icon-Font CSS-Class: This field is used for a Font-Icon. Fill in the class name here, for example for Font Awesome: fas fa-check'";
 TCMSLogChange::RunQuery(__LINE__, $query);
 
-$data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
-  ->setFields([
-      'name' => 'icon_font_css_class',
-      'translation' => 'Icon-Font CSS-Klasse',
-      'cms_field_type_id' => TCMSLogChange::GetFieldType('CMSFIELD_STRING'),
-      '049_helptext' => 'In diesem Feld kann ein Font-Icon angegeben werden. Tragen Sie hier den Klassennamen ein, z.B. für Font Awesome: fas fa-check',
-  ])
-  ->setWhereEquals([
-      'id' => '6a8eb0c7-bb06-e930-f29b-d6e9073fc1d2',
-  ])
-;
-TCMSLogChange::update(__LINE__, $data);
-
 $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'en')
     ->setFields([
 //        'name' => 'icon_font_css_class',
-        'translation' => 'Icon-Font CSS-Class',
-        '049_helptext' => 'This field is used for a Font-Icon. Fill in the class name here, for example for Font Awesome: fas fa-check',
+        'translation' => 'Icon font CSS class',
+        '049_helptext' => 'This field is used for a font icon. Fill in the class name here, for example for Font Awesome: fas fa-check',
     ])
     ->setWhereEquals([
-        'id' => '6a8eb0c7-bb06-e930-f29b-d6e9073fc1d2',
+        'id' => $recordId,
     ])
 ;
 TCMSLogChange::update(__LINE__, $data);
