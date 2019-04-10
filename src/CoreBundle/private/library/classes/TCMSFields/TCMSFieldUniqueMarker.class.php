87ca158b3e8c0b4ac8570c4cbb8ec1a75a418f1b
Stefan Kruppa <stefan@umulmrum.de>
2019-04-10 13:58:09 +0200

    ref #325: Fix recording of queries with where expressions; fix recording for unique fields

--- src/CoreBundle/private/library/classes/TCMSFields/TCMSFieldUniqueMarker.class.php
+++ src/CoreBundle/private/library/classes/TCMSFields/TCMSFieldUniqueMarker.class.php
@@ -24,46 +24,67 @@ class TCMSFieldUniqueMarker extends TCMSFieldBoolean
     public function PreGetSQLHook()
     {
         // we need to check if this record is set as the unique record and reset all other records
-        if ('1' == $this->ConvertPostDataToSQL()) {
-            $oTableConf = &$this->oTableRow->GetTableConf();
-
-            $setFields = array();
-            $whereConditions = array();
-            $updateQuery = 'UPDATE `'.MySqlLegacySupport::getInstance()->real_escape_string($oTableConf->sqlData['name']).'` SET `'.MySqlLegacySupport::getInstance()->real_escape_string($this->name)."` = '0' WHERE `id` != '".MySqlLegacySupport::getInstance()->real_escape_string($this->oTableRow->id)."' AND `".MySqlLegacySupport::getInstance()->real_escape_string($this->name)."` = '1' ";
-            $setFields[$this->name] = '0';
-            $whereConditions[] = new Comparison('id', Comparison::NEQ, $this->oTableRow->id);
-            $whereConditions[] = new Comparison($this->name, Comparison::EQ, '1');
-
-            // check if the table has a foreign key field
-            /**
-             * @var TdbCmsFieldConfList $oFields
-             */
-            $oFields = $oTableConf->GetFieldDefinitions(array('CMSFIELD_PROPERTY_PARENT_ID'), true);
-            if (1 == $oFields->Length()) {
-                $oField = $oFields->Current();
-                $databaseConnection = $this->getDatabaseConnection();
-                $quotedNameField = $databaseConnection->quoteIdentifier($oField->sqlData['name']);
-                $quotedNameValue = $databaseConnection->quote($this->oTableRow->sqlData[$oField->sqlData['name']]);
-                $updateQuery .= " AND $quotedNameField = $quotedNameValue ";
-                $whereConditions[] = new Comparison($oField->sqlData['name'], Comparison::EQ, $this->oTableRow->sqlData[$oField->sqlData['name']]);
-            }
-
-            // also allow restricting based on a field in the current row
-            $sRestriction = $this->oDefinition->GetFieldtypeConfigKey('restriction');
-            if (!empty($sRestriction) && array_key_exists($sRestriction, $this->oTableRow->sqlData)) {
-                $updateQuery .= ' AND `'.MySqlLegacySupport::getInstance()->real_escape_string($oTableConf->sqlData['name']).'`.`'.MySqlLegacySupport::getInstance()->real_escape_string($sRestriction)."` = '".MySqlLegacySupport::getInstance()->real_escape_string($this->oTableRow->sqlData[$sRestriction])."'";
-            }
-
-            MySqlLegacySupport::getInstance()->query($updateQuery);
-
-            $editLanguage = $this->getLanguageService()->getActiveEditLanguage();
-            $migrationQueryData = new MigrationQueryData($oTableConf->sqlData['name'], $editLanguage->fieldIso6391);
-            $migrationQueryData->setFields($setFields);
-            $migrationQueryData->setWhereExpressions($whereConditions);
-
-            $aQuery = array(new LogChangeDataModel($migrationQueryData, LogChangeDataModel::TYPE_UPDATE));
-            TCMSLogChange::WriteTransaction($aQuery);
+        if ('1' != $this->ConvertPostDataToSQL()) {
+            return true;
         }
+
+        $oTableConf = &$this->oTableRow->GetTableConf();
+        $databaseConnection = $this->getDatabaseConnection();
+
+        $setFields = [];
+        $whereEquals = [];
+        $whereExpressions = [];
+
+        $quotedTableName = $databaseConnection->quoteIdentifier($oTableConf->sqlData['name']);
+        $quotedFieldName = $databaseConnection->quoteIdentifier($this->name);
+
+        $updateQuery = "UPDATE $quotedTableName SET $quotedFieldName = '0' WHERE `id` != ? AND $quotedFieldName = '1' ";
+        $parameters = [
+            $this->oTableRow->id,
+        ];
+        $setFields[$this->name] = '0';
+        $whereExpressions[] = new Comparison('id', Comparison::NEQ, $this->oTableRow->id);
+        $whereEquals[$this->name] = '1';
+
+        // check if the table has a foreign key field
+        /**
+         * @var TdbCmsFieldConfList $oFields
+         */
+        $oFields = $oTableConf->GetFieldDefinitions(array('CMSFIELD_PROPERTY_PARENT_ID'), true);
+        if (1 === $oFields->Length()) {
+            $oField = $oFields->Current();
+            $nameField = $oField->sqlData['name'];
+            $quotedNameField = $databaseConnection->quoteIdentifier($nameField);
+            $nameValue = $this->oTableRow->sqlData[$nameField];
+
+            $updateQuery .= " AND $quotedNameField = ? ";
+            $parameters[] = $nameValue;
+
+            $whereEquals[$nameField] = $nameValue;
+        }
+
+        // also allow restricting based on a field in the current row
+        $sRestriction = $this->oDefinition->GetFieldtypeConfigKey('restriction');
+        if (!empty($sRestriction) && array_key_exists($sRestriction, $this->oTableRow->sqlData)) {
+            $quotedRestrictionField = $databaseConnection->quoteIdentifier($sRestriction);
+            $restrictionValue = $this->oTableRow->sqlData[$sRestriction];
+
+            $updateQuery .= " AND $quotedRestrictionField = ? ";
+            $parameters[] = $restrictionValue;
+
+            $whereEquals[$sRestriction] = $this->oTableRow->sqlData[$sRestriction];
+        }
+
+        $databaseConnection->executeUpdate($updateQuery, $parameters);
+
+        $editLanguage = $this->getLanguageService()->getActiveEditLanguage();
+        $migrationQueryData = new MigrationQueryData($oTableConf->sqlData['name'], $editLanguage->fieldIso6391);
+        $migrationQueryData->setFields($setFields);
+        $migrationQueryData->setWhereEquals($whereEquals);
+        $migrationQueryData->setWhereExpressions($whereExpressions);
+
+        $aQuery = array(new LogChangeDataModel($migrationQueryData, LogChangeDataModel::TYPE_UPDATE));
+        TCMSLogChange::WriteTransaction($aQuery);
 
         return true;
     }
