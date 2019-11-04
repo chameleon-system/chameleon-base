12f4402c1564bfdca9d57d08b17aac5f4d5b5826
Carmen Kleis <carmen.kleis@esono.de>
2019-11-04 13:40:31 +0100

    ref #107: navigation-tree: drag&drop, connect&disconnect pages

--- src/CoreBundle/private/modules/CMSModulePageTree/views/standard.view.php
+++ src/CoreBundle/private/modules/CMSModulePageTree/views/standard.view.php
@@ -1,60 +1,4 @@
 <?php
-    /**
-     * @var \Symfony\Component\Translation\TranslatorInterface
-     */
-    $translator = \ChameleonSystem\CoreBundle\ServiceLocator::get('translator');
-?>
-
-<script type="text/javascript">
-    var currentNodeID = null;
-
-    /**
-     * moves node by drag&drop data
-     */
-    function moveNode(nodeID, parentNodeID, position) {
-        if (typeof parentNodeID != 'undefined' && typeof nodeID != 'undefined') {
-            CHAMELEON.CORE.showProcessingModal();
-            var url = '<?=PATH_CMS_CONTROLLER; ?>?<?=TTools::GetArrayAsURLForJavascript(array('pagedef' => 'CMSModulePageTree', 'module_fnc' => array('contentmodule' => 'ExecuteAjaxCall'), '_fnc' => 'MoveNode', 'tableid' => $data['treeTableID'])); ?>&nodeID=' + nodeID + '&parentNodeID=' + parentNodeID + '&position=' + position;
-            GetAjaxCallTransparent(url, moveNodeSuccess);
-        }
-    }
-
-    /**
-     * unblocks the UI
-     */
-    function moveNodeSuccess(nodeID, responseMessage) {
-        window.parent.CHAMELEON.CORE.hideProcessingModal();
-    }
-
-
-    function assignPage(node) {
-        var nodeID = $(node).attr('esrealid');
-        var assignedDataID = '<?php if (!empty($data['dataID'])) {
-    echo $data['dataID'];
-} ?>';
-        var url = '<?=PATH_CMS_CONTROLLER; ?>?<?=TTools::GetArrayAsURLForJavascript(array('tableid' => $data['treeNodeTableID'], 'pagedef' => 'tableeditorPopup', 'sRestrictionField' => 'cms_tree_id', 'module_fnc' => array('contentmodule' => 'Insert'), 'active' => '1', 'preventTemplateEngineRedirect' => '1')); ?>&sRestriction=' + nodeID + '&contid=' + assignedDataID + '&cms_tree_id=' + nodeID;
-        CreateModalIFrameDialogCloseButton(url);
-    }
-
-    /**
-     * changes the display of the current node after assign dialog
-     */
-    function TreeNodeAssignFormResponse(data, responseMessage) {
-        if (data) {
-            CloseModalIFrameDialog();
-            if (data.assigned) {
-                $('#' + data.treeNodeID + ' span').addClass('activeConnectedNode');
-            } else {
-                $('#' + data.treeNodeID + ' span').removeClass('activeConnectedNode');
-            }
-        }
-    }
-
-
-</script>
-
-
-<?php
 
 /**
  * @var \ChameleonSystem\CoreBundle\DataModel\BackendTreeNodeDataModel $treeNodes
@@ -69,6 +13,7 @@
  * @var string $openTreeNodeEditorAddNewNodeUrl
  * @var string $deleteNodeUrl
  * @var string $assignPageUrl
+ * @var string $moveNodeUrl
  * @var object $dataID
  */
 $viewRenderer = new ViewRenderer();
@@ -82,6 +27,7 @@ $viewRenderer->AddSourceObject('openTreeNodeEditorUrl', $openTreeNodeEditorUrl);
 $viewRenderer->AddSourceObject('openTreeNodeEditorAddNewNodeUrl', $openTreeNodeEditorAddNewNodeUrl);
 $viewRenderer->AddSourceObject('deleteNodeUrl', $deleteNodeUrl);
 $viewRenderer->AddSourceObject('assignPageUrl', $assignPageUrl);
+$viewRenderer->AddSourceObject('moveNodeUrl', $moveNodeUrl);
 $viewRenderer->AddSourceObject('pageId', $dataID);
 
 echo $viewRenderer->Render('CMSModulePageTree/standard.html.twig');
