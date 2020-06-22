6a7ea37599e4127b222b221d5b5fae61ed7cebe4
UlrichKu <ulrich.kuhn@esono.de>
2020-06-22 11:38:06 +0200

    Revert "557 enable resource collection in backend"

--- src/CoreBundle/private/modules/CMSTemplateEngine/CMSTemplateEngine.class.php
+++ src/CoreBundle/private/modules/CMSTemplateEngine/CMSTemplateEngine.class.php
@@ -553,8 +553,6 @@ class CMSTemplateEngine extends TCMSModelBase
       </script>";
         }
 
-        $aIncludes[] = '<script src="'.TGlobal::GetStaticURLToWebLib('/components/select2.v4/js/select2.full.min.js').'" type="text/javascript"></script>';
-
         return $aIncludes;
     }
 
@@ -564,6 +562,7 @@ class CMSTemplateEngine extends TCMSModelBase
     public function GetHtmlFooterIncludes()
     {
         $aIncludes = parent::GetHtmlFooterIncludes();
+        $aIncludes[] = '<script src="'.TGlobal::GetStaticURLToWebLib('/components/select2.v4/js/select2.full.min.js').'" type="text/javascript"></script>';
 
         if ('cmp_loadmoduleinstance' == $this->aModuleConfig['view']) {
             $aChooseModuleViewDialog = $this->getChooseModuleViewDialog();
