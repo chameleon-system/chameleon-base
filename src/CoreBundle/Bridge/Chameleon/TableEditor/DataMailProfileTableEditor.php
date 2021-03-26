307fad90cd0f398f8dd957d5abba873395845e11
Koray Zorluoglu <koray.zorluoglu@esono.de>
2021-03-26 12:08:24 +0100

    ref #695: New DataMailProfileTableEditor with migration

---
+++ src/CoreBundle/Bridge/Chameleon/TableEditor/DataMailProfileTableEditor.php
@@ -0,0 +1,65 @@
+<?php
+namespace ChameleonSystem\CoreBundle\Bridge\Chameleon\TableEditor;
+
+
+use ChameleonSystem\CoreBundle\Interfaces\FlashMessageServiceInterface;
+use ChameleonSystem\CoreBundle\ServiceLocator;
+use Psr\Log\LoggerInterface;
+use TCMSTableEditor;
+use Twig\Error\SyntaxError;
+use Twig\Environment;
+use Twig\Source;
+
+class DataMailProfileTableEditor extends TCMSTableEditor
+{
+    const MESSAGE_MANAGER_CONSUMER = 'DataMailProfileTableEditorMessages';
+
+    protected function DataIsValid(&$postData, $oFields = null)
+    {
+        $twig = $this->getTwigEnvironment();
+        try {
+            $twig->tokenize(new Source($postData['body'], 'body'));
+            $twig->tokenize(new Source($postData['body_text'], 'body_text'));
+        }catch (SyntaxError $e) {
+
+            $this->getLogger()->error(
+                sprintf('failed to pares body field in E-Mail Template %s', $postData['name']),
+                [
+                    'sFieldName' => 'body',
+                    'message' => $e->getMessage(),
+                    'guess' => $e->guess(),
+                    'context' => $e->getSourceContext()->getCode(),
+                    'exception' => $e,
+                ]
+            );
+
+            $this->getFlashMessageService()->addMessage(
+                self::MESSAGE_MANAGER_CONSUMER,
+                'chameleon_system.table_editor_twig_error',
+                [
+                    'sFieldName' => 'body',
+                    'message' => $e->getMessage(),
+                    'guess' => $e->guess(),
+                ]
+            );
+            return false;
+        }
+
+        return parent::DataIsValid($postData, $oFields);
+    }
+
+    private function getFlashMessageService(): FlashMessageServiceInterface
+    {
+        return ServiceLocator::get('chameleon_system_core.flash_messages');
+    }
+
+    private function getTwigEnvironment(): Environment
+    {
+        return ServiceLocator::get('twig');
+    }
+
+    private function getLogger(): LoggerInterface
+    {
+        return ServiceLocator::get('logger');
+    }
+}
