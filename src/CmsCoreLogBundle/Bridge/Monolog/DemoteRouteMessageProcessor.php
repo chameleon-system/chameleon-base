dddfa967310b4869fb9e315a47ca40831b0599b7
Ulrich Kuhn <ulrich.kuhn@esono.de>
2022-02-02 10:15:03 +0100

    ref #760: Change the message level of the route match message.

---
+++ src/CmsCoreLogBundle/Bridge/Monolog/DemoteRouteMessageProcessor.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Chameleon System (https://www.chameleonsystem.com).
+ *
+ * (c) ESONO AG (https://www.esono.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ChameleonSystem\CmsCoreLogBundle\Bridge\Monolog;
+
+use Monolog\Logger;
+use Monolog\Processor\ProcessorInterface;
+
+class DemoteRouteMessageProcessor implements ProcessorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(array $record)
+    {
+        if ('request' !== $record['channel']) {
+            return $record;
+        }
+
+        if (Logger::INFO !== $record['level']) {
+            return $record;
+        }
+
+        if (0 !== \mb_strpos($record['message'], 'Matched route ')) {
+            return $record;
+        }
+
+        $record['level'] = Logger::DEBUG;
+
+        return $record;
+    }
+}
