1ac38db843e57a2b8220a3044e470aa77e283c9e
Ulrich Kuhn <ulrich.kuhn@esono.de>
2022-04-14 09:34:38 +0200

    ref #751: Allow easy disabling of jquery

--- src/CoreBundle/EventListener/AddJqueryIncludeListener.php
+++ src/CoreBundle/EventListener/AddJqueryIncludeListener.php
@@ -21,18 +21,22 @@ class AddJqueryIncludeListener
      */
     public function onGlobalHtmlHeaderInclude(HtmlIncludeEventInterface $event)
     {
+        $jqueryInclude = '';
         if (true === TGlobal::IsCMSMode()) {
-            $sInclude = '<script src="'.TGlobal::GetStaticURL('/chameleon/blackbox/javascript/jquery/jquery-3.3.1.min.js').'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
-            $sInclude .= '<script src="'.TGlobal::GetStaticURL('/chameleon/blackbox/javascript/jquery/jquery-migrate-1.4.1.js').'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
+            $jqueryInclude = '<script src="'.TGlobal::GetStaticURL('/chameleon/blackbox/javascript/jquery/jquery-3.3.1.min.js').'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
+            $jqueryInclude .= '<script src="'.TGlobal::GetStaticURL('/chameleon/blackbox/javascript/jquery/jquery-migrate-1.4.1.js').'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
         } else {
             if (defined('CHAMELEON_URL_GOOGLE_JQUERY') && CHAMELEON_URL_GOOGLE_JQUERY !== false) {
-                $sInclude = '<script src="'.CHAMELEON_URL_GOOGLE_JQUERY.'"></script>';
+                $jqueryInclude = '<script src="'.CHAMELEON_URL_GOOGLE_JQUERY.'"></script>';
                 $sJQueryLocal = '<script src="'.TGlobal::GetStaticURL(CHAMELEON_URL_JQUERY).'" type="text/javascript"></script>';
-                $sInclude .= "\n<script>window.jQuery || document.write('".str_replace('/', '\\/', $sJQueryLocal)."')</script>";
-            } else {
-                $sInclude = '<script src="'.TGlobal::GetStaticURL(CHAMELEON_URL_JQUERY).'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
+                $jqueryInclude .= "\n<script>window.jQuery || document.write('".str_replace('/', '\\/', $sJQueryLocal)."')</script>";
+            } else if (CHAMELEON_URL_JQUERY !== false) {
+                $jqueryInclude = '<script src="'.TGlobal::GetStaticURL(CHAMELEON_URL_JQUERY).'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
             }
         }
-        $event->addData(array($sInclude));
+
+        if ('' !== $jqueryInclude) {
+            $event->addData([$jqueryInclude]);
+        }
     }
 }
