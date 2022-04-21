ef2e2eaa4afe1bddd03243178ba744b6d18de55c
Ulrich Kuhn <ulrich.kuhn@esono.de>
2022-04-21 12:30:24 +0200

    ref #751: Add data in any "case"

--- src/CoreBundle/EventListener/AddJqueryIncludeListener.php
+++ src/CoreBundle/EventListener/AddJqueryIncludeListener.php
@@ -21,22 +21,23 @@ class AddJqueryIncludeListener
      */
     public function onGlobalHtmlHeaderInclude(HtmlIncludeEventInterface $event)
     {
-        $jqueryInclude = '';
         if (true === TGlobal::IsCMSMode()) {
             $jqueryInclude = '<script src="'.TGlobal::GetStaticURL('/chameleon/blackbox/javascript/jquery/jquery-3.3.1.min.js').'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
             $jqueryInclude .= '<script src="'.TGlobal::GetStaticURL('/chameleon/blackbox/javascript/jquery/jquery-migrate-1.4.1.js').'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
+
+            $event->addData([$jqueryInclude]);
         } else {
             if (defined('CHAMELEON_URL_GOOGLE_JQUERY') && CHAMELEON_URL_GOOGLE_JQUERY !== false) {
                 $jqueryInclude = '<script src="'.CHAMELEON_URL_GOOGLE_JQUERY.'"></script>';
                 $sJQueryLocal = '<script src="'.TGlobal::GetStaticURL(CHAMELEON_URL_JQUERY).'" type="text/javascript"></script>';
                 $jqueryInclude .= "\n<script>window.jQuery || document.write('".str_replace('/', '\\/', $sJQueryLocal)."')</script>";
+
+                $event->addData([$jqueryInclude]);
             } else if (CHAMELEON_URL_JQUERY !== false) {
                 $jqueryInclude = '<script src="'.TGlobal::GetStaticURL(CHAMELEON_URL_JQUERY).'" type="text/javascript"></script><!--#GLOBALRESOURCECOLLECTION#-->';
-            }
-        }
 
-        if ('' !== $jqueryInclude) {
-            $event->addData([$jqueryInclude]);
+                $event->addData([$jqueryInclude]);
+            }
         }
     }
 }
