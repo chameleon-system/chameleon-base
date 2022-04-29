a0d1cb03e360af2140fe43cfbcb8d71e6c0b8943
Ulrich Kuhn <ulrich.kuhn@esono.de>
2022-04-29 09:39:35 +0200

    ref #656: Refactor creation code

--- src/CoreBundle/Service/GuidCreationService.php
+++ src/CoreBundle/Service/GuidCreationService.php
@@ -22,21 +22,22 @@ class GuidCreationService implements GuidCreationServiceInterface
     public function findUnusedId(string $tableName): string
     {
         $quotedTableName = $this->connection->quoteIdentifier($tableName);
-        $tries = 11;
+        $tries = 10;
 
         do {
             $id = \TTools::GetUUID();
             try {
                 $count = $this->connection->fetchOne("SELECT count(*) FROM $quotedTableName WHERE `id` = :id", ['id' => $id]);
+                if (false !== $count) {
+                    $count = \intval($count);
+                }
             } catch (Exception $exception) {
                 throw new GuidCreationFailedException('GuidCreationService cannot create an unused ID for '.$tableName, $exception->getCode(), $exception);
             }
             --$tries;
         } while (false !== $count && $count > 0 && $tries > 0);
 
-        if (0 === $tries) {
-            // TODO this can run 11 times and if the last one succeeds this exeception is thrown
-
+        if (0 !== $count) {
             throw new GuidCreationFailedException('GuidCreationService was unable to create an unused ID after some attempts for '.$tableName);
         }
 
