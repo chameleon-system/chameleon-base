ab0c0bae65f1411b3f445c96da2f8f63dee37fdf
Ulrich Kuhn <ulrich.kuhn@esono.de>
2022-05-05 08:42:02 +0200

    ref #772: Remove unwanted (and unusable) code

--- src/PkgCoreBundle/objects/TPkgCmsCoreSendToHost.class.php
+++ src/PkgCoreBundle/objects/TPkgCmsCoreSendToHost.class.php
@@ -160,30 +160,6 @@ class TPkgCmsCoreSendToHost
     {
         if (null === $this->lastResponseHeaderVariables) {
             $this->lastResponseHeaderVariables = array();
-
-            /**
-             * @psalm-suppress UndefinedClass
-             * @var resource $rHeader
-             * @see https://github.com/chameleon-system/chameleon-system/issues/772
-             */
-            $rHeader = TPkgCmsStringUtilities::getStringStream($this->getLastResponseHeader());
-
-            $this->lastResponseCodeRaw = fgets($rHeader);
-
-            $aTmp = explode("\n", $this->getLastResponseHeader());
-            foreach ($aTmp as $sHeaderLine) {
-                $iSplit = strpos($sHeaderLine, ':');
-                $sHeaderVarName = trim(substr($sHeaderLine, 0, $iSplit));
-                $sHeaderVarValue = trim(substr($sHeaderLine, $iSplit));
-                if (isset($this->lastResponseHeaderVariables[$sHeaderVarName])) {
-                    if (!is_array($this->lastResponseHeaderVariables[$sHeaderVarName])) {
-                        $this->lastResponseHeaderVariables[$sHeaderVarName] = array($this->lastResponseHeaderVariables[$sHeaderVarName]);
-                    }
-                    $this->lastResponseHeaderVariables[$sHeaderVarName][] = $sHeaderVarValue;
-                } else {
-                    $this->lastResponseHeaderVariables[$sHeaderVarName] = $sHeaderVarValue;
-                }
-            }
         }
 
         if (isset($sVarName, $this->lastResponseHeaderVariables)) {
