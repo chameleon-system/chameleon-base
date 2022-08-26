85cb48e5af36399b7ccd7810df1142afb72e0465
Johannes Hertenstein <git@j6s.dev>
2022-08-26 15:49:12 +0200

    ref #766: Remove test strings from configuration loading

--- src/CoreBundle/DependencyInjection/ChameleonSystemCoreExtension.php
+++ src/CoreBundle/DependencyInjection/ChameleonSystemCoreExtension.php
@@ -218,8 +218,8 @@ class ChameleonSystemCoreExtension extends Extension
         $definition->setArgument(0, $geocoderConfig['geo_json_endpoint']);
 
         $container->setParameter('chameleon_system_core.geocoding.attribution.show', $geocoderConfig['attribution']['show']);
-        $container->setParameter('chameleon_system_core.geocoding.attribution.name', 'test' . $geocoderConfig['attribution']['name']);
-        $container->setParameter('chameleon_system_core.geocoding.attribution.url', 'test' . $geocoderConfig['attribution']['url']);
+        $container->setParameter('chameleon_system_core.geocoding.attribution.name', $geocoderConfig['attribution']['name']);
+        $container->setParameter('chameleon_system_core.geocoding.attribution.url', $geocoderConfig['attribution']['url']);
     }
 
     /**
