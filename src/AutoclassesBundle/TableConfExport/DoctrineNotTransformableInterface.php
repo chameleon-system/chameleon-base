a40d287ca4542953ca4a26d4b9e8de00f3d8ae2a
Robert Nürnberg <robert.nuernberg@esono.de>
2023-03-02 15:05:52 +0100

    ref #59244: add interface to marke fields that cannnot be transformed

---
+++ src/AutoclassesBundle/TableConfExport/DoctrineNotTransformableInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ChameleonSystem\AutoclassesBundle\TableConfExport;
+
+// indicate, that the field can not be transformed. added so we can easily see which fields have been completed
+interface DoctrineNotTransformableInterface
+{
+
+}
