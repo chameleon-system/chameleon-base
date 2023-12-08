a61ff80cea953a00d8065f8dba1ad1043ab5eb03
Robert Nürnberg <robert.nuernberg@esono.de>
2023-12-08 16:13:33 +0100

    ref #59178: made sure, that domain_to_base_user_mapping is always configured

--- src/SecurityBundle/src/DependencyInjection/ChameleonSystemSecurityExtension.php
+++ src/SecurityBundle/src/DependencyInjection/ChameleonSystemSecurityExtension.php
@@ -26,9 +26,15 @@ class ChameleonSystemSecurityExtension extends ConfigurableExtension
         $loginController = $container->getDefinition(CmsLoginController::class);
         $loginController->setArgument('$enableGoogleLogin', true);
         $newUserConfig = $container->getDefinition(GoogleUserRegistrationService::class);
-        $newUserConfig->setArgument('$domainToBaseUserMapping', $mergedConfig['google_login']['domain_to_base_user_mapping']);
+        $newUserConfig->setArgument(
+            '$domainToBaseUserMapping',
+            $mergedConfig['google_login']['domain_to_base_user_mapping'] ?? []
+        );
 
         $authenticator = $container->getDefinition(GoogleAuthenticator::class);
-        $authenticator->setArgument('$allowedDomains', array_keys($mergedConfig['google_login']['domain_to_base_user_mapping']));
+        $authenticator->setArgument(
+            '$allowedDomains',
+            array_keys($mergedConfig['google_login']['domain_to_base_user_mapping'] ?? [])
+        );
     }
 }
