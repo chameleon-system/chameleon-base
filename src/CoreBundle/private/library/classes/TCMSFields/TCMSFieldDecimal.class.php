84e3d084deeee9a59210fd335a771a846ce4cc35
Robert Nürnberg <robert.nuernberg@esono.de>
2023-12-28 09:23:28 +0100

    ref #59244: decimal needs to be string as doctrine handles these as string for precision

--- src/CoreBundle/private/library/classes/TCMSFields/TCMSFieldDecimal.class.php
+++ src/CoreBundle/private/library/classes/TCMSFields/TCMSFieldDecimal.class.php
@@ -33,11 +33,12 @@ class TCMSFieldDecimal extends TCMSField implements DoctrineTransformableInterfa
     {
         $parameters = [
             'source' => get_class($this),
-            'type' => 'float',
-            'docCommentType' => 'float',
+            // doctrine handles decimals as string
+            'type' => 'string',
+            'docCommentType' => 'string',
             'description' => $this->oDefinition->sqlData['translation'],
             'propertyName' => $this->snakeToPascalCase($this->name),
-            'defaultValue' => sprintf('%s', (float)$this->oDefinition->sqlData['field_default_value']),
+            'defaultValue' => sprintf("'%s'", (string)$this->oDefinition->sqlData['field_default_value']),
             'allowDefaultValue' => true,
             'getterName' => 'get'. $this->snakeToCamelCase($this->name),
             'setterName' => 'set'. $this->snakeToCamelCase($this->name),
