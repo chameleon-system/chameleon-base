54cf1a27a8b47df7f371b00896dfb1d29c969cee
Sven Jansen <sven.jansen@esono.de>
2024-06-03 13:26:41 +0200

    ref #63689: clean up code

--- src/CoreBundle/Service/RequestInfoService.php
+++ src/CoreBundle/Service/RequestInfoService.php
@@ -158,13 +158,11 @@ class RequestInfoService implements RequestInfoServiceInterface
         // todo: `__previewmode` should be the only way to enable this. Refactor all places where the preview attribute is set as `preview` instead of `__previewmode'
         // @note: preview mode laoding only via cookie is not possible, because this will kill all frontend logins
         $this->isPreviewModeCache = false === \TGlobal::IsCMSMode()
+            && true === $this->getPreviewModeService()->currentSessionHasPreviewAccess()
             && (
-                true === $this->getPreviewModeService()->currentSessionHasPreviewAccess()
-                && (
-                    'true' === $request->query->get('__previewmode')
-                    || 'true' === $request->query->get('preview')
-                )
-            );
+                'true' === $request->query->get('__previewmode')
+                 || 'true' === $request->query->get('preview')
+                );
 
         return $this->isPreviewModeCache;
     }
