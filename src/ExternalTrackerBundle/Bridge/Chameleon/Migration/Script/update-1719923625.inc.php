6d5fab435addb74663dc522b3748985e534cff2c
Sven Jansen <sven.jansen@esono.de>
2024-07-02 17:29:47 +0200

    ref #63934: add updates to create pkg_external_tracker table if missing

--- src/ExternalTrackerBundle/Bridge/Chameleon/Migration/Script/update-1719923625.inc.php
+++ src/ExternalTrackerBundle/Bridge/Chameleon/Migration/Script/update-1719923625.inc.php
@@ -7,21 +7,11 @@
 if (false === TCMSLogChange::RecordExists('pkg_cms_class_manager','name_of_entry_point', 'TPkgExternalTrackerState')) {
     $data = TCMSLogChange::createMigrationQueryData('pkg_cms_class_manager', 'en')
         ->setFields([
-            'name_of_entry_point' => '',
-            'exit_class' => '',
+            'name_of_entry_point' => 'TPkgExternalTrackerState',
+            'exit_class' => 'TPkgExternalTrackerStateEndPoint',
             'exit_class_subtype' => '',
             'exit_class_type' => 'Core',
             'id' => '421f31a9-a34c-8517-5255-7ae30970562b',
         ]);
     TCMSLogChange::insert(__LINE__, $data);
-
-    $data = TCMSLogChange::createMigrationQueryData('pkg_cms_class_manager', 'en')
-        ->setFields([
-            'name_of_entry_point' => 'TPkgExternalTrackerState',
-            'exit_class' => 'TPkgExternalTrackerStateEndPoint',
-        ])
-        ->setWhereEquals([
-            'id' => '421f31a9-a34c-8517-5255-7ae30970562b',
-        ]);
-    TCMSLogChange::update(__LINE__, $data);
 }
