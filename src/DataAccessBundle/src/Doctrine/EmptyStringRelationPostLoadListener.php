bd4940c2cd3b156d5edd5ae145b73c732f00ca0f
Matthias Derer <matthias.derer@esono.de>
2024-10-11 10:47:04 +0200

    ref #64585: fix handling of empty relation fields

---
+++ src/DataAccessBundle/src/Doctrine/EmptyStringRelationPostLoadListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ChameleonSystem\DataAccessBundle\Doctrine;
+
+use Doctrine\ORM\Event\PostLoadEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+// In chameleon, foreign key fields representing no relation are stored as empty strings instead of using null to indicate no relation.
+// This listener will set the relation to null if the foreign key field is an empty string. Otherwise, doctrine will assume a relation
+// exists with the id '' and try to load the related entity resulting in an exception when accessing the relation.
+class EmptyStringRelationPostLoadListener
+{
+
+    public function postLoad(PostLoadEventArgs $event)
+    {
+        $metadata = $event->getObjectManager()->getClassMetadata($event->getObject()::class);
+        $object = $event->getObject();
+        foreach ($metadata->getAssociationMappings() as $mapping) {
+            if ($mapping['type'] !== ClassMetadataInfo::MANY_TO_ONE) {
+                continue;
+            }
+            $name = $mapping['fieldName'];
+            $relation = $object->{'get' . ucfirst($name)}();
+            if ($relation->getId() === '') {
+                $object->{'set' . ucfirst($name)}(null);
+            }
+        }
+
+        return $event;
+    }
+
+}
