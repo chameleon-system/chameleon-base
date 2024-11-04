0441b1e94b05ddf5846d579b714beae3150f715a
Carmen Kleis <carmen.kleis@esono.de>
2024-11-04 15:06:33 +0100

    ref #64864: get cmsVersion from composer.lock

--- src/CoreBundle/Resources/config/version.inc.php
+++ src/CoreBundle/Resources/config/version.inc.php
@@ -1,4 +1,19 @@
 <?php
 
-define('CMS_VERSION_MAJOR', '7');
-define('CMS_VERSION_MINOR', '1.4');
+use ChameleonSystem\CoreBundle\Bridge\Chameleon\Twig\VersionExtension;
+
+/**
+ * @deprecated since 7.1.48 - use VersionExtension directly
+ */
+$versionExtension = new VersionExtension();
+$cmsVersion = $versionExtension->getCmsVersion();
+
+if (false !== strpos($cmsVersion, '.')) {
+    [$major, $minor] = explode('.', $cmsVersion, 2);
+} else {
+    $major = 'Major-Version-not-found';
+    $minor = 'Minor-Version-not-found';
+}
+
+define('CMS_VERSION_MAJOR', $major);
+define('CMS_VERSION_MINOR', $minor);
