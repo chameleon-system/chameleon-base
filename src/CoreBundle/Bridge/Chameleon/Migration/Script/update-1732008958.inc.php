bd93994fbc330dd36d72743dcb7595fc20d67031
Sven Jansen <sven.jansen@esono.de>
2024-11-20 11:07:51 +0000

    ref #65030: fix field position target field

--- src/CoreBundle/Bridge/Chameleon/Migration/Script/update-1732008958.inc.php
+++ src/CoreBundle/Bridge/Chameleon/Migration/Script/update-1732008958.inc.php
@@ -10,10 +10,7 @@ $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
         //'cms_tbl_conf_id' => TCMSLogChange::GetTableId('cms_user'),
         //'name' => 'preview_token',
         //'translation' => 'Vorschau Token',
-        //'position' => 0, // is set below
         'modifier' => 'readonly',
-        'fieldclass_subtype' => '',
-        'class_type' => 'Core',
     ])
     ->setWhereEquals([
         'id' => 'f492ced8-aa48-b9bb-a294-1c422c361913',
@@ -21,4 +18,4 @@ $data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
 ;
 TCMSLogChange::update(__LINE__, $data);
 
-TCMSLogChange::SetFieldPosition(TCMSLogChange::GetTableId('cms_user'), 'preview_token', 'date_modified');
+TCMSLogChange::SetFieldPosition(TCMSLogChange::GetTableId('cms_user'), 'preview_token', 'show_as_rights_template');
