1efcdeda523fd0d3dcc463cdcbd4585a23d2d067
Michael Michaelis <michael.michaelis@esono.de>
2024-11-26 13:55:22 +0100

    ref #65094: lil modzns

--- src/CoreBundle/Util/UrlNormalization/UrlNormalizerSpecialChars.php
+++ src/CoreBundle/Util/UrlNormalization/UrlNormalizerSpecialChars.php
@@ -16,7 +16,7 @@ class UrlNormalizerSpecialChars implements UrlNormalizerInterface
     /**
      * @var array<string, string>
      */
-    private static $specialCharNormalization = array(
+    private static array $specialCharNormalization = [
         '®' => '',
         '™' => '',
         '€' => ' EUR',
@@ -114,7 +114,7 @@ class UrlNormalizerSpecialChars implements UrlNormalizerInterface
         'Þ' => '',
         '|' => '',
         '\\' => '',
-    );
+    ];
 
     /**
      * The third index in the search array is a special character. It looks like a space but it is a special space
@@ -123,7 +123,7 @@ class UrlNormalizerSpecialChars implements UrlNormalizerInterface
      *
      * @var string[]
      */
-    private static $specialCharToSpacerNormalization = array(
+    private static array $specialCharToSpacerNormalization = [
         '/',
         ' ',
         ' ',
@@ -131,7 +131,7 @@ class UrlNormalizerSpecialChars implements UrlNormalizerInterface
         '.',
         '&',
         '…',
-    );
+    ];
 
     /**
      * {@inheritdoc}
