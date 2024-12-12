84be51980f8c7cc2124de16b0a573fea6af5fa62
Michael Michaelis <michael.michaelis@esono.de>
2024-12-12 18:11:21 +0100

    ref #64654: inject session storage configuration to get session lifetime

--- src/CoreBundle/Service/PreviewModeService.php
+++ src/CoreBundle/Service/PreviewModeService.php
@@ -9,11 +9,16 @@ use TTools;
 class PreviewModeService implements PreviewModeServiceInterface
 {
     public const COOKIE_NAME = 'preview_mode';
+    private const DEFAULT_PREVIEW_COOKIE_LIFETIME = 3 * 86400; // 3 days
 
+    /**
+     * @param array<string, mixed> $sessionOptions
+     */
     public function __construct(
         protected readonly string $hashingSecret,
         protected readonly Connection $connection,
         protected readonly TTools $tools,
+        protected readonly array $sessionOptions,
     ) {
     }
 
@@ -56,10 +61,15 @@ class PreviewModeService implements PreviewModeServiceInterface
             }
             $token = $this->tools::GetUUID();
             $this->connection->update('cms_user', ['preview_token' => $token], ['id' => $cmsUserId]);
-            setcookie(self::COOKIE_NAME, $token.'|'.$this->generateHash($token), time() + 86400, '/', '', false, true);
+            setcookie(self::COOKIE_NAME, $token.'|'.$this->generateHash($token), time() + $this->getSessionLifetime(), '/', '', false, true);
         } catch (Exception) {
             // ignore if field not exists yet
         }
+    }
+
+    protected function getSessionLifetime(): int
+    {
+        return $this->sessionOptions['cookie_lifetime'] ?? self::DEFAULT_PREVIEW_COOKIE_LIFETIME;
     }
 
     protected function generateHash(string $toHash): string
