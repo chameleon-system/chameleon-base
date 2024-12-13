2ba9a5a8011dcc2de4e2140b79889a96f12c51b8
Sven Jansen <sven.jansen@esono.de>
2024-12-13 18:26:55 +0100

    ref #65286: remove deprecated autoFlushing feature

--- src/SecurityBundle/src/Controller/CmsLoginController.php
+++ src/SecurityBundle/src/Controller/CmsLoginController.php
@@ -4,7 +4,6 @@ namespace ChameleonSystem\SecurityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
