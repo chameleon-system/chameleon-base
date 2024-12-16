afd8d90aebe3e5c43871209ceea3d1f97d0d50bb
Carmen Kleis <carmen.kleis@esono.de>
2024-12-16 11:31:28 +0100

    ref #65243: code improvements in default.layout.php

--- src/CoreBundle/private/rendering/layouts/default.layout.php
+++ src/CoreBundle/private/rendering/layouts/default.layout.php
@@ -3,11 +3,11 @@
 use ChameleonSystem\CoreBundle\ServiceLocator;
 $authenticityTokenManager = ServiceLocator::get('chameleon_system_core.security.authenticity_token.authenticity_token_manager');
 
-$oViewRender = new ViewRenderer();
-$oViewRender->AddSourceObject('cmsauthenticitytokenParameter', $authenticityTokenManager->getTokenPlaceholderAsParameter());
-echo $oViewRender->Render('BackendLayout/layout.html.twig');
+$viewRender = new ViewRenderer();
+$viewRender->AddSourceObject('cmsauthenticitytokenParameter', $authenticityTokenManager->getTokenPlaceholderAsParameter());
+echo $viewRender->Render('BackendLayout/layout.html.twig');
 
 // message garbage collection
 // dirty hack to prevent message shown on wrong table editor or table list instance
-$oMessageManager = TCMSMessageManager::GetInstance();
-$oMessageManager->ClearMessages();
+$flashMessage = ServiceLocator::get('chameleon_system_core.flash_messages');
+$flashMessage->ClearMessages();
