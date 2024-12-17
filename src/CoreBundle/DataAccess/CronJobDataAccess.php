2dbfb244fa515e55f693f1eb7c4057eeccee179a
Sven Jansen <sven.jansen@esono.de>
2024-12-17 08:22:38 +0100

    ref #65294: fix missing cronjob data access

---
+++ src/CoreBundle/DataAccess/CronJobDataAccess.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ChameleonSystem\CoreBundle\DataAccess;
+
+class CronJobDataAccess
+{
+    private const CRONJOB_TIMESTAMP_PARAMETER = 'cronjob_last_call_timestamp';
+
+    public function getTimestampOfLastCronJobCall(): int
+    {
+        $cmsConfig = \TCMSConfig::GetInstance();
+
+        if (null === $cmsConfig) {
+            return 0;
+        }
+
+        $lastCronJobCall = $cmsConfig->GetConfigParameter(self::CRONJOB_TIMESTAMP_PARAMETER, false, true);
+
+        return null === $lastCronJobCall ? 0 : (int) $lastCronJobCall;
+    }
+
+    public function refreshTimestampOfLastCronJobCall(): void
+    {
+        $cmsConfig = \TCMSConfig::GetInstance();
+
+        if (null === $cmsConfig) {
+            return;
+        }
+
+        $cmsConfig->SetConfigParameter(self::CRONJOB_TIMESTAMP_PARAMETER, time(), true);
+    }
+
+}
