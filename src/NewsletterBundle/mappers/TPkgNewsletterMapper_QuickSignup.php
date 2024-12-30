b09a1ad398623a81baee5ad1e3c6c28fcba7e934
Sven Jansen <sven.jansen@esono.de>
2024-12-30 09:06:23 +0100

    ref #65248: add TPkgNewsletterMapper_QuickSignup stub again to prevent breaking autoclass generation

---
+++ src/NewsletterBundle/mappers/TPkgNewsletterMapper_QuickSignup.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Chameleon System (https://www.chameleonsystem.com).
+ *
+ * (c) ESONO AG (https://www.esono.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @deprecated since 6.1.0 - will be removed in 9.0
+ */
+class TPkgNewsletterMapper_QuickSignup extends AbstractViewMapper
+{
+    public function GetRequirements(IMapperRequirementsRestricted $oRequirements)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function Accept(IMapperVisitorRestricted $oVisitor, $bCachingEnabled, IMapperCacheTriggerRestricted $oCacheTriggerManager)
+    {
+    }
+}
