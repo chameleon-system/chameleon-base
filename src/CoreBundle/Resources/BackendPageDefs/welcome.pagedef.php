4156cfe66d1fef5ce2bfe2031fc21432e22ef8e3
Sven Jansen <sven.jansen@esono.de>
2025-01-08 11:33:37 +0100

    ref #65182: use dashboard as default welcome module, remove unused StaticView

--- src/CoreBundle/Resources/BackendPageDefs/welcome.pagedef.php
+++ src/CoreBundle/Resources/BackendPageDefs/welcome.pagedef.php
@@ -1,14 +1,21 @@
 <?php
+/*
+ * This file is part of the Chameleon System (https://www.chameleonsystem.com).
+ *
+ * (c) ESONO AG (https://www.esono.de)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 $layoutTemplate = 'default';
 $moduleList = [
     'contentmodule' => [
-        'model' => 'chameleon_system_core.module.static_view_module',
-        'view' => 'standard',
-        'targetView' => 'StaticView/[{language}]/cms_welcome.html.twig',
+        'model' => 'chameleon_system_cms_dashboard.backend_module.dashboard',
+        'moduleType' => '@ChameleonSystemCmsDashboardBundle',
+        'view' => 'dashboard',
     ],
 ];
-
 addDefaultPageTitle($moduleList);
 addDefaultHeader($moduleList);
 addDefaultBreadcrumb($moduleList);
