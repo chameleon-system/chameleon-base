05597549a54002336b5d54eb5f6e7b3100693142
Michael Michaelis <michael.michaelis@esono.de>
2025-01-17 18:19:20 +0100

    ref #65489: rights changed for role triggers affected user to reload theirs rights and roles

--- src/CoreBundle/private/library/classes/TCMSTableEditor/TCMSTableEditorCMSRole.class.php
+++ src/CoreBundle/private/library/classes/TCMSTableEditor/TCMSTableEditorCMSRole.class.php
@@ -9,11 +9,36 @@
  * file that was distributed with this source code.
  */
 
+use Doctrine\DBAL\Exception as DBALException;
+
 /**
  * add a menu item to switch to any user if the current user has that right.
 /**/
 class TCMSTableEditorCMSRole extends TCMSTableEditor
 {
+    protected function PostSaveHook($oFields, $oPostTable)
+    {
+        parent::PostSaveHook($oFields, $oPostTable);
+        $this->rightsChanged();
+    }
+
+    /**
+     * @see \ChameleonSystem\SecurityBundle\CmsUser\CmsUserDataAccess::userHasBeenModified
+     */
+    protected function rightsChanged(): void
+    {
+        $query = 'UPDATE `cms_user`
+            INNER JOIN `cms_user_cms_role_mlt` ON `cms_user_cms_role_mlt`.source_id = `cms_user`.`id`
+            SET `date_modified` = :date_time 
+            WHERE `cms_user_cms_role_mlt`.`target_id` = :role_id';
+
+        try {
+            $this->getDatabaseConnection()->executeQuery($query, ['date_time' => date('Y-m-d H:i:s'), 'role_id' => $this->sId]);
+        } catch (DBALException) {
+            return;
+        }
+    }
+
     /**
      * add table-specific buttons to the editor (add them directly to $this->oMenuItems).
      */
