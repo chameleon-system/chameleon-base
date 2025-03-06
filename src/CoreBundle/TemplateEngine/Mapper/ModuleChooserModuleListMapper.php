fed5911d4fb244b43f55c81e98303c6ea90da514
Sven Jansen <sven.jansen@esono.de>
2025-03-06 15:48:57 +0100

    ref #65920: refactor ModuleChooserModuleListMapper

--- src/CoreBundle/TemplateEngine/Mapper/ModuleChooserModuleListMapper.php
+++ src/CoreBundle/TemplateEngine/Mapper/ModuleChooserModuleListMapper.php
@@ -1,28 +1,13 @@
 <?php
 
-/*
- * This file is part of the Chameleon System (https://www.chameleonsystem.com).
- *
- * (c) ESONO AG (https://www.esono.de)
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace ChameleonSystem\CoreBundle\TemplateEngine\Mapper;
 
-use AbstractViewMapper;
-use IMapperCacheTriggerRestricted;
-use IMapperRequirementsRestricted;
-use IMapperVisitorRestricted;
-use TGlobal;
-
-class ModuleChooserModuleListMapper extends AbstractViewMapper
+class ModuleChooserModuleListMapper extends \AbstractViewMapper
 {
     /**
      * {@inheritdoc}
      */
-    public function GetRequirements(IMapperRequirementsRestricted $oRequirements): void
+    public function GetRequirements(\IMapperRequirementsRestricted $oRequirements): void
     {
         $oRequirements->NeedsSourceObject('moduleList', 'TdbCmsTplModuleList');
         $oRequirements->NeedsSourceObject('aPermittedModules', 'array', null, true);
@@ -32,17 +17,27 @@ class ModuleChooserModuleListMapper extends AbstractViewMapper
      * {@inheritdoc}
      */
     public function Accept(
-        IMapperVisitorRestricted $oVisitor,
+        \IMapperVisitorRestricted $oVisitor,
         $bCachingEnabled,
-        IMapperCacheTriggerRestricted $oCacheTriggerManager
+        \IMapperCacheTriggerRestricted $oCacheTriggerManager
     ): void {
-        /** @var array $permittedModules */
         $permittedModules = $oVisitor->GetSourceObject('aPermittedModules');
 
         /** @var \TdbCmsTplModuleList $moduleList */
         $moduleList = $oVisitor->GetSourceObject('moduleList');
 
-        $modules = array();
+        $modules = $this->getModules($moduleList, $permittedModules);
+
+        $data = [
+            'iconPath' => \TGlobal::GetStaticURLToWebLib('/'),
+            'modules' => $modules,
+        ];
+        $oVisitor->SetMappedValueFromArray($data);
+    }
+
+    private function getModules(\TdbCmsTplModuleList $moduleList, ?array $permittedModules): array
+    {
+        $modules = [];
         $moduleList->GoToStart();
         $moduleList->bAllowItemCache = true;
         while ($moduleObject = $moduleList->Next()) {
@@ -52,39 +47,43 @@ class ModuleChooserModuleListMapper extends AbstractViewMapper
             if (is_array($permittedModules) && false === isset($permittedModules[$moduleObject->fieldClassname])) {
                 continue;
             }
-            $module = array(
-                'name' => $moduleObject->fieldName,
-                'id' => $moduleObject->id,
-                'icon' => $moduleObject->fieldIconFontCssClass,
-                'views' => array(),
-            );
-            $moduleObject->aPermittedViews = $permittedModules[$moduleObject->fieldClassname] ?? null;
-            /** @var \TIterator<string> $views */
-            $views = $moduleObject->GetViews();
-            if (0 === $views->Length()) {
-                continue;
-            }
-            $viewMapping = $moduleObject->GetViewMapping();
-
-            $views->GoToStart();
-            while ($viewName = $views->Next()) {
-                $displayName = (is_array($viewMapping) && isset($viewMapping[$viewName])) ? $viewMapping[$viewName] : $viewName;
-                $module['views'][] = array(
-                    'systemName' => $viewName,
-                    'displayName' => $displayName,
-                );
-            }
-            usort($module['views'], function ($element1, $element2) {
-                return $element1['displayName'] > $element2['displayName'] ? 1 : 0;
-            });
-
-            $modules[] = $module;
+            $modules[] = $this->getModuleData($moduleObject, $permittedModules);
         }
 
-        $data = array(
-            'iconPath' => TGlobal::GetStaticURLToWebLib('/'),
-            'modules' => $modules,
-        );
-        $oVisitor->SetMappedValueFromArray($data);
+        return $modules;
+    }
+
+    private function getModuleData(\TdbCmsTplModule $moduleObject, ?array $permittedModules): array
+    {
+        $module = [
+            'name' => $moduleObject->fieldName,
+            'id' => $moduleObject->id,
+            'views' => [],
+        ];
+        $moduleObject->aPermittedViews = $permittedModules[$moduleObject->fieldClassname] ?? [];
+        $views = $moduleObject->GetViews();
+        if (null !== $views && 0 !== $views->Length()) {
+            $module['views'] = $this->getModuleViews($views, $moduleObject->GetViewMapping());
+        }
+
+        return $module;
+    }
+
+    private function getModuleViews($views, $viewMapping): array
+    {
+        $moduleViews = [];
+        $views->GoToStart();
+        while ($viewName = $views->Next()) {
+            $displayName = (is_array($viewMapping) && isset($viewMapping[$viewName])) ? $viewMapping[$viewName] : $viewName;
+            $moduleViews[] = [
+                'systemName' => $viewName,
+                'displayName' => $displayName,
+            ];
+        }
+        usort($moduleViews, function ($element1, $element2) {
+            return $element1['displayName'] > $element2['displayName'] ? 1 : 0;
+        });
+
+        return $moduleViews;
     }
 }
