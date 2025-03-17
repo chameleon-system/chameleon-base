0b515a71f6d1a6ca7ce77a298e4bb86793cc1d8d
Markus Haendler <markus.haendler@esono.de>
2025-03-17 12:51:37 +0100

    ref #66056: WIP remove name translation field

---
+++ src/CoreBundle/Bridge/Chameleon/Migration/Script/update-1742210913.inc.php
@@ -0,0 +1,25 @@
+<h1>Build #1742210913</h1>
+<h2>Date: 2025-03-17</h2>
+<div class="changelog">
+    - removed translateable from name field of table cms_right
+</div>
+<?php
+
+$data = TCMSLogChange::createMigrationQueryData('cms_field_conf', 'de')
+    ->setFields([
+        'is_translatable' => '0', // prev.: '1'
+    ])
+    ->setWhereEquals([
+        'name' => 'name',
+        'cms_tbl_conf_id' => TCMSLogChange::GetTableId('cms_right'),
+    ])
+;
+TCMSLogChange::update(__LINE__, $data);
+
+$query ="ALTER TABLE cms_right
+                     CHANGE name
+                            name VARCHAR(40) NOT NULL COMMENT 'CMS-Kürzel des Rechtetyps: '";
+TCMSLogChange::RunQuery(__LINE__, $query);
+
+$query ="ALTER TABLE cms_right DROP name__en";
+TCMSLogChange::RunQuery(__LINE__, $query);
