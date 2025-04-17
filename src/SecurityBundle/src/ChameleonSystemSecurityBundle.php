e7065770c7aa5e534b30bfcacb08abb023a95874
Markus Haendler <markus.haendler@esono.de>
2025-04-17 16:13:50 +0200

    ref #64588: mphp-cs-fixer

--- src/SecurityBundle/src/ChameleonSystemSecurityBundle.php
+++ src/SecurityBundle/src/ChameleonSystemSecurityBundle.php
@@ -2,8 +2,6 @@
 
 namespace ChameleonSystem\SecurityBundle;
 
-use ChameleonSystem\SecurityBundle\DependencyInjection\ChamleeonSystemSecurityTwoFactorCompilerPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ChameleonSystemSecurityBundle extends Bundle
