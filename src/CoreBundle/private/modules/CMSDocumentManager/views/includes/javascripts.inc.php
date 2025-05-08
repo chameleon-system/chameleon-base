63250ac6ac57f0a6628a167461055fced9a835d9
Sven Jansen <sven.jansen@esono.de>
2025-05-08 12:22:38 +0200

    ref #66526: set active document folder in headline

--- src/CoreBundle/private/modules/CMSDocumentManager/views/includes/javascripts.inc.php
+++ src/CoreBundle/private/modules/CMSDocumentManager/views/includes/javascripts.inc.php
@@ -5,7 +5,7 @@
      */
     function CreateNode(obj) {
         var id = obj.attr("id").replace("node", "");
-        var url = '<?=PATH_CMS_CONTROLLER; ?>?tableid=<?=$data['treeTableID']; ?>&amp;pagedef=tableeditorPopup&amp;module_fnc%5Bcontentmodule%5D=Insert&amp;parent_id=' + id;
+        var url = '<?php echo PATH_CMS_CONTROLLER; ?>?tableid=<?php echo $data['treeTableID']; ?>&amp;pagedef=tableeditorPopup&amp;module_fnc%5Bcontentmodule%5D=Insert&amp;parent_id=' + id;
         CreateModalIFrameDialogCloseButton(url);
     }
 
@@ -14,7 +14,7 @@
      *
      */
     function editFileDetails(id, row) {
-        url = '<?=PATH_CMS_CONTROLLER; ?>?tableid=<?=$data['id']; ?>&pagedef=tableeditorPopup&id=' + id;
+        url = '<?php echo PATH_CMS_CONTROLLER; ?>?tableid=<?php echo $data['id']; ?>&pagedef=tableeditorPopup&id=' + id;
         CreateModalIFrameDialogCloseButton(url);
     }
 
@@ -23,18 +23,24 @@
 
     function showFileList(nodeID) {
         randomKey = parseInt(Math.random() * (1000 - 1 + 1));
-        var url = "<?=PATH_CMS_CONTROLLER; ?>?pagedef=tableManagerDocumentManager&id=<?=$data['id']; ?><?php if (!empty($data['recordID'])) {
-    echo '&mltTable='.$data['mltTable'].'&recordID='.$data['recordID'];
-} ?>&_listName=cmstablelistObj<?=$data['cmsident']; ?>";
-        if (nodeID && nodeID != '1') {
+        let url = "<?php echo PATH_CMS_CONTROLLER; ?>?pagedef=tableManagerDocumentManager&id=<?php echo $data['id']; ?><?php if (!empty($data['recordID'])) {
+            echo '&mltTable='.$data['mltTable'].'&recordID='.$data['recordID'];
+        } ?>&_listName=cmstablelistObj<?php echo $data['cmsident']; ?>";
+        if (nodeID && nodeID !== '1') {
             url += '&sRestrictionField=cms_document_tree_id&sRestriction=' + nodeID;
         }
         url += '&randkey=' + randomKey;
 
-        //refresh parent header
-
         document.getElementById('fileList').src = url;
+
+        let nodeTreeNode = document.getElementById('node' + nodeID);
+        let folderName = nodeTreeNode.querySelector('a')?.textContent?.trim();
+
+        if (folderName) {
+            document.getElementById('selectedFolderTitle').textContent = folderName;
+        }
     }
+
 
     /*
      * save selected files to paste them to a directory
@@ -42,9 +48,9 @@
      */
     function cutSelectedFiles() {
         if (selectedFiles.length > 0) {
-            toasterMessage('<?php echo TGlobal::OutJS(\ChameleonSystem\CoreBundle\ServiceLocator::get('translator')->trans('chameleon_system_core.document_manager.msg_select_target_folder')); ?>', 'WARNING');
+            toasterMessage('<?php echo TGlobal::OutJS(ChameleonSystem\CoreBundle\ServiceLocator::get('translator')->trans('chameleon_system_core.document_manager.msg_select_target_folder')); ?>', 'WARNING');
         } else {
-            toasterMessage('<?php echo TGlobal::OutJS(\ChameleonSystem\CoreBundle\ServiceLocator::get('translator')->trans('chameleon_system_core.document_manager.msg_no_files_selected_for_move')); ?>', 'WARNING');
+            toasterMessage('<?php echo TGlobal::OutJS(ChameleonSystem\CoreBundle\ServiceLocator::get('translator')->trans('chameleon_system_core.document_manager.msg_no_files_selected_for_move')); ?>', 'WARNING');
         }
     }
 
@@ -56,10 +62,10 @@
     function PasteFiles(selectedNodeID) {
         if (selectedFiles.length != 0) {
             var filesCommaList = selectedFiles.join(",");
-            var url = '<?=PATH_CMS_CONTROLLER; ?>?pagedef=CMSDocumentManager&module_fnc[contentmodule]=ExecuteAjaxCall&_fnc=PasteFiles&documents=' + filesCommaList + '&nodeID=' + selectedNodeID;
+            var url = '<?php echo PATH_CMS_CONTROLLER; ?>?pagedef=CMSDocumentManager&module_fnc[contentmodule]=ExecuteAjaxCall&_fnc=PasteFiles&documents=' + filesCommaList + '&nodeID=' + selectedNodeID;
             GetAjaxCall(url, PasteFilesResponse);
         } else {
-            toasterMessage('<?php echo TGlobal::OutJS(\ChameleonSystem\CoreBundle\ServiceLocator::get('translator')->trans('chameleon_system_core.document_manager.msg_no_files_selected_for_insert')); ?>', 'WARNING');
+            toasterMessage('<?php echo TGlobal::OutJS(ChameleonSystem\CoreBundle\ServiceLocator::get('translator')->trans('chameleon_system_core.document_manager.msg_no_files_selected_for_insert')); ?>', 'WARNING');
         }
     }
 
@@ -92,13 +98,12 @@
     function deleteSelectedItem() {
 
         if (selectedFiles.length != 0) {
-            var url = "<?=PATH_CMS_CONTROLLER; ?>?pagedef=CMSDocumentManager&module_fnc[contentmodule]=ExecuteAjaxCall&_fnc=CheckConnectionsAndDelete&documentIDs=" + selectedFiles;
+            var url = "<?php echo PATH_CMS_CONTROLLER; ?>?pagedef=CMSDocumentManager&module_fnc[contentmodule]=ExecuteAjaxCall&_fnc=CheckConnectionsAndDelete&documentIDs=" + selectedFiles;
             GetAjaxCall(url, deleteSelectedItemResponse);
         }
         //refresh parent header
         RefreshOpenerHeader();
     }
-
 
     function deleteSelectedItemResponse(data, responseMessage) {
         CloseModalIFrameDialog();
@@ -114,7 +119,6 @@
             DeleteFormCallback();
         }
     }
-
 
     function ShowConnectionDialog(index) {
         CreateModalIFrameDialogFromContent(_connectedDataHTML[index], 0, 0, CHAMELEON.CORE.i18n.Translate('chameleon_system_core.js.delete_document'));
