786c334a5d0a013b30a254424a9282ca9f7660fb
Hadi Noureddine <hadi.noureddine@esono.de>
2025-09-18 10:49:23 +0200

    ref #67799: add getLink method to DocumentFileDownload class

--- src/MarkdownCmsBundle/Bridge/Chameleon/Entity/DocumentFileDownload.php
+++ src/MarkdownCmsBundle/Bridge/Chameleon/Entity/DocumentFileDownload.php
@@ -15,4 +15,9 @@ class DocumentFileDownload extends \ChameleonSystemMarkdownCmsBundleBridgeChamel
 
         return $fileName;
     }
+
+    public function getLink($bAbsolute = false, $sAnchor = null, $aOptionalParameters = [], ?\TdbCmsPortal $portal = null, ?\TdbCmsLanguage $language = null)
+    {
+        return $this->GetPlainDownloadLink(false, false, !$bAbsolute);
+    }
 }
