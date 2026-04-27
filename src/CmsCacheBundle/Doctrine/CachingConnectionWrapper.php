709d1dd2feb187fc3d5f0042c8ccb441c5299ef0
Markus Haendler <markus.haendler@esono.de>
2026-04-27 17:27:13 +0200

    ref #70143: remove comment block

--- src/CmsCacheBundle/Doctrine/CachingConnectionWrapper.php
+++ src/CmsCacheBundle/Doctrine/CachingConnectionWrapper.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Chameleon System (https://www.chameleonsystem.com).
- *
- * (c) ESONO AG (https://www.esono.de)
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace ChameleonSystem\CmsCacheBundle\Doctrine;
 
 use ChameleonSystem\CmsCacheBundle\DataModel\QueryCacheDecision;
