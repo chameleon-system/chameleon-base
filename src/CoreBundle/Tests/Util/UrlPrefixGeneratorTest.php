eb46694fbd2d4090835ff0c8acc6f5e8361968d1
Markus Haendler <markus.haendler@esono.de>
2026-08-04 10:26:36 +0200

    - ref #70945: implement caching for the urlprefixgenerator

--- src/CoreBundle/Tests/Util/UrlPrefixGeneratorTest.php
+++ src/CoreBundle/Tests/Util/UrlPrefixGeneratorTest.php
@@ -39,4 +39,80 @@ class UrlPrefixGeneratorTest extends TestCase
         $portal->fieldUseMultilanguage = false;
         self::assertSame('/shop', $generator->generatePrefix($portal, $french, $domain));
     }
+
+    public function testGetLanguagePrefixCachesPrimaryDomainLookupPerLanguageAndDomainCombination(): void
+    {
+        $portalDomainService = $this->createMock(PortalDomainServiceInterface::class);
+        $portalDomainService->expects(self::once())
+            ->method('getPrimaryDomain')
+            ->with('portal-1', 'fr')
+            ->willReturn($this->createPrimaryDomain('de'));
+
+        $languageService = $this->createMock(LanguageServiceInterface::class);
+        $generator = new UrlPrefixGenerator($portalDomainService, $languageService);
+
+        $portal = $this->createPortal('portal-1', true, '', 'shop');
+        $language = $this->createLanguage('fr', 'fr');
+
+        self::assertSame('fr', $generator->getLanguagePrefix($portal, $language));
+        self::assertSame('fr', $generator->getLanguagePrefix($portal, $language));
+    }
+
+    public function testGetLanguagePrefixCachesDomainLanguageListLookup(): void
+    {
+        $portalDomainService = $this->createMock(PortalDomainServiceInterface::class);
+        $portalDomainService->expects(self::never())->method('getPrimaryDomain');
+
+        $languageService = $this->createMock(LanguageServiceInterface::class);
+        $generator = new UrlPrefixGenerator($portalDomainService, $languageService);
+
+        $portal = $this->createPortal('portal-1', true, 'de', 'shop');
+        $domain = $this->getMockBuilder('TdbCmsPortalDomains')
+            ->disableAutoload()
+            ->onlyMethods(['GetFieldCmsLanguageIdList'])
+            ->getMock();
+        $domain->id = 'domain-1';
+        $domain->fieldCmsLanguageId = '';
+        $domain->expects(self::once())
+            ->method('GetFieldCmsLanguageIdList')
+            ->willReturn(['fr']);
+
+        $language = $this->createLanguage('fr', 'fr');
+
+        self::assertSame('fr', $generator->getLanguagePrefix($portal, $language, $domain));
+        self::assertSame('fr', $generator->getLanguagePrefix($portal, $language, $domain));
+    }
+
+    private function createPortal(
+        string $id,
+        bool $useMultilanguage,
+        string $defaultLanguageId,
+        string $identifier
+    ): \TdbCmsPortal {
+        $portal = $this->getMockBuilder('TdbCmsPortal')->disableAutoload()->getMock();
+        $portal->id = $id;
+        $portal->fieldUseMultilanguage = $useMultilanguage;
+        $portal->fieldCmsLanguageId = $defaultLanguageId;
+        $portal->fieldIdentifier = $identifier;
+
+        return $portal;
+    }
+
+    private function createLanguage(string $id, string $isoCode, string $urlPrefix = ''): \TdbCmsLanguage
+    {
+        $language = $this->getMockBuilder('TdbCmsLanguage')->disableAutoload()->getMock();
+        $language->id = $id;
+        $language->fieldIso6391 = $isoCode;
+        $language->fieldUrlPrefix = $urlPrefix;
+
+        return $language;
+    }
+
+    private function createPrimaryDomain(string $languageId): \TdbCmsPortalDomains
+    {
+        $domain = $this->getMockBuilder('TdbCmsPortalDomains')->disableAutoload()->getMock();
+        $domain->fieldCmsLanguageId = $languageId;
+
+        return $domain;
+    }
 }
